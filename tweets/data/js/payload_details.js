bdeea72f727850b7a4bd91e819cd3bdea8c0281e
mhawksey <m.hawksey@gmail.com>
2017-08-12 02:16:02 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26050,
-	"created_at": "Fri Aug 11 01:16:00 +0000 2017",
+	"tweets": 26051,
+	"created_at": "Sat Aug 12 01:16:00 +0000 2017",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2017_08.js
+++ tweets/data/js/tweets/2017_08.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Fri Aug 11 16:03:44 +0000 2017",
+		"id": 896039438899392500,
+		"id_str": "896039438899392514",
+		"text": "RT @ivankutil: https://t.co/tRRvk1j0Z9\n\nCAD software in web browser https://t.co/imKUiClhmA",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ivankutil",
+					"name": "Ivan Kutil",
+					"id": 171220030,
+					"id_str": "171220030",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tRRvk1j0Z9",
+					"expanded_url": "https://openjscad.org/",
+					"display_url": "openjscad.org",
+					"indices": [
+						15,
+						38
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 895914442181025800,
+					"id_str": "895914442181025792",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+					"url": "https://t.co/imKUiClhmA",
+					"display_url": "pic.twitter.com/imKUiClhmA",
+					"expanded_url": "https://twitter.com/ivankutil/status/895914512091664384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 522,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 999,
+							"h": 767,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 999,
+							"h": 767,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 895914512091664400,
+					"source_status_id_str": "895914512091664384",
+					"source_user_id": 171220030,
+					"source_user_id_str": "171220030"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 895914442181025800,
+					"id_str": "895914442181025792",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+					"url": "https://t.co/imKUiClhmA",
+					"display_url": "pic.twitter.com/imKUiClhmA",
+					"expanded_url": "https://twitter.com/ivankutil/status/895914512091664384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 522,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 999,
+							"h": 767,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 999,
+							"h": 767,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 895914512091664400,
+					"source_status_id_str": "895914512091664384",
+					"source_user_id": 171220030,
+					"source_user_id_str": "171220030"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Community & Technology Officer @A_L_T | Google Expert Apps Script | Interested in Open Education, EdTech, Mashups, Analytics, Data, Networks, \\o/",
+			"url": "http://t.co/l60kPN56DA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/l60kPN56DA",
+							"expanded_url": "http://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7245,
+			"friends_count": 1778,
+			"listed_count": 579,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 3239,
+			"utc_offset": 3600,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25849,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/68812920/Untitled-1.gif",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/68812920/Untitled-1.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Aug 11 07:47:19 +0000 2017",
+			"id": 895914512091664400,
+			"id_str": "895914512091664384",
+			"text": "https://t.co/tRRvk1j0Z9\n\nCAD software in web browser https://t.co/imKUiClhmA",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/tRRvk1j0Z9",
+						"expanded_url": "https://openjscad.org/",
+						"display_url": "openjscad.org",
+						"indices": [
+							0,
+							23
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 895914442181025800,
+						"id_str": "895914442181025792",
+						"indices": [
+							53,
+							76
+						],
+						"media_url": "http://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+						"url": "https://t.co/imKUiClhmA",
+						"display_url": "pic.twitter.com/imKUiClhmA",
+						"expanded_url": "https://twitter.com/ivankutil/status/895914512091664384/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 522,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 999,
+								"h": 767,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 999,
+								"h": 767,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 895914442181025800,
+						"id_str": "895914442181025792",
+						"indices": [
+							53,
+							76
+						],
+						"media_url": "http://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DG7tcDtXoAAEdxI.jpg",
+						"url": "https://t.co/imKUiClhmA",
+						"display_url": "pic.twitter.com/imKUiClhmA",
+						"expanded_url": "https://twitter.com/ivankutil/status/895914512091664384/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 522,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 999,
+								"h": 767,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 999,
+								"h": 767,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 171220030,
+				"id_str": "171220030",
+				"name": "Ivan Kutil",
+				"screen_name": "ivankutil",
+				"location": "Czech republic, Europe",
+				"description": "Google Developer Expert (GDE) for Apps Script • CTO @AppSatori • AI & machine learning • API • math & science • JavaScript • Cloud • Progressive Web Apps",
+				"url": "https://t.co/nju9cnB814",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/nju9cnB814",
+								"expanded_url": "http://kutil.org",
+								"display_url": "kutil.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 320,
+				"friends_count": 171,
+				"listed_count": 10,
+				"created_at": "Mon Jul 26 21:12:40 +0000 2010",
+				"favourites_count": 30,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 288,
+				"lang": "cs",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/666816679435874304/NtbuF_rI_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/666816679435874304/NtbuF_rI_normal.png",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 6,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Aug 10 21:45:42 +0000 2017",
 		"id": 895763111533215700,
 		"id_str": "895763111533215745",
