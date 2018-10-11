cd23078d89c032f78a447e8c5ba2beb98dac296f
mhawksey <m.hawksey@gmail.com>
2018-10-11 16:58:31 +0100

    Revert "Added 2 tweets"
    This reverts commit a93f28676f1103031e7f4a2d8f6bb353e1dcc0d9.

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,10 @@
 var payload_details = {
+<<<<<<< HEAD
 	"tweets": 27632,
 	"created_at": "Thu Oct 11 15:28:00 +0000 2018",
+=======
+	"tweets": 27593,
+	"created_at": "Sat Sep 29 01:16:02 +0000 2018",
+>>>>>>> parent of a93f286... Added 2 tweets
 	"lang": "en"
 }
