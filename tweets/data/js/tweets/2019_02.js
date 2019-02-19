30e78314d9f9b04979353aece1488673c70cdf6c
mhawksey <m.hawksey@gmail.com>
2019-02-19 01:10:53 +0000

    Added 2 tweets

--- tweets/data/js/tweets/2019_02.js
+++ tweets/data/js/tweets/2019_02.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 17 05:10:02 +0000 2019",
+		"id": 1097000197043904500,
+		"id_str": "1097460533333241857",
+		"full_text": "Walkthrough of how I created the App Maker + Google Hangouts Chat Bot as mentioned in the Totally Unscripted episode with @mhawksey a couple weeks ago: https://t.co/x1Z62G0H63  #gsuite #appmaker #appsscript",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			206
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "gsuite",
+					"indices": [
+						177,
+						184
+					]
+				},
+				{
+					"text": "appmaker",
+					"indices": [
+						185,
+						194
+					]
+				},
+				{
+					"text": "appsscript",
+					"indices": [
+						195,
+						206
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						122,
+						131
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/x1Z62G0H63",
+					"expanded_url": "http://www.cinfourthirds.com/using-the-hangout-chat-bot-api-for-an-app-maker-app/",
+					"display_url": "cinfourthirds.com/using-the-hang…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1868737826,
+			"id_str": "1868737826",
+			"name": "Cleo",
+			"screen_name": "CinFourThirds",
+			"location": "Edmonton, AB",
+			"description": "Google Developer Expert for G-Suite. Occasional photographer and traveler",
+			"url": "https://t.co/FxiHAL5EkA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FxiHAL5EkA",
+							"expanded_url": "http://www.cinfourthirds.com",
+							"display_url": "cinfourthirds.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35,
+			"friends_count": 34,
+			"listed_count": 2,
+			"created_at": "Sun Sep 15 18:29:38 +0000 2013",
+			"favourites_count": 38,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 68,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1056294429718384640/8pHCpVRa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1056294429718384640/8pHCpVRa_normal.jpg",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Walkthrough of how I created the App Maker + Google Hangouts Chat Bot as mentioned in the Totally Unscripted episode with @mhawksey a couple weeks ago: https://t.co/x1Z62G0H63  #gsuite #appmaker #appsscript"
+	},
+	{
+		"created_at": "Mon Feb 18 09:11:57 +0000 2019",
+		"id": 1097423463806046200,
+		"id_str": "1097423463806046208",
+		"full_text": "@barrielroberts Thanks Baz - glad you find useful (we use this in a number of projects, mostly when sending emails with links)",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "barrielroberts",
+					"name": "Baz Roberts",
+					"id": 4409428666,
+					"id_str": "4409428666",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1097134290394120200,
+		"in_reply_to_status_id_str": "1097134290394120194",
+		"in_reply_to_user_id": 4409428666,
+		"in_reply_to_user_id_str": "4409428666",
+		"in_reply_to_screen_name": "barrielroberts",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Community & Technology Officer @A_L_T | @GoogleDevExpert Apps Script | Interested in Open Education, EdTech, Mashups, Analytics, Data, Networks, \\o/",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7967,
+			"friends_count": 2160,
+			"listed_count": 592,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 5066,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 27838,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@barrielroberts Thanks Baz - glad you find useful (we use this in a number of projects, mostly when sending emails with links)"
+	},
+	{
 		"created_at": "Sat Feb 16 07:53:05 +0000 2019",
 		"id": 1096678840506691600,
 		"id_str": "1096678840506691584",
