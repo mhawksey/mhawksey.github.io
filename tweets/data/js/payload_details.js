014b93a3a86353e945702eef98e104c76977fce4
mhawksey <m.hawksey@gmail.com>
2019-04-03 02:10:52 +0100

    Added 3 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28209,
-	"created_at": "Tue Apr 02 01:10:52 +0000 2019",
+	"tweets": 28212,
+	"created_at": "Wed Apr 03 01:10:51 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_04.js
+++ tweets/data/js/tweets/2019_04.js
@@ -1,6 +1,508 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 03 00:26:35 +0000 2019",
+		"id": 1113059170788360200,
+		"id_str": "1113236316421013504",
+		"full_text": "We are now recruiting for @A_L_T 's next Membership Manager and Administration Officer to join the #altc staff team - find out more about working with us https://t.co/zkvEcYZKYG https://t.co/YW1nPUE50n",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "altc",
+					"indices": [
+						99,
+						104
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "A_L_T",
+					"name": "ALT - alt.ac.uk",
+					"id": 41210876,
+					"id_str": "41210876",
+					"indices": [
+						26,
+						32
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zkvEcYZKYG",
+					"expanded_url": "https://www.alt.ac.uk/about-alt/work-us",
+					"display_url": "alt.ac.uk/about-alt/work…",
+					"indices": [
+						154,
+						177
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1113059168565518300,
+					"id_str": "1113059168565518338",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/D3JhaqRW4AI2yZQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3JhaqRW4AI2yZQ.jpg",
+					"url": "https://t.co/YW1nPUE50n",
+					"display_url": "pic.twitter.com/YW1nPUE50n",
+					"expanded_url": "https://twitter.com/A_L_T/status/1113059170788360193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1312,
+							"h": 1321,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 675,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1192,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1113059168565518300,
+					"id_str": "1113059168565518338",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/D3JhaqRW4AI2yZQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3JhaqRW4AI2yZQ.jpg",
+					"url": "https://t.co/YW1nPUE50n",
+					"display_url": "pic.twitter.com/YW1nPUE50n",
+					"expanded_url": "https://twitter.com/A_L_T/status/1113059170788360193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1312,
+							"h": 1321,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 675,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1192,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 41210876,
+			"id_str": "41210876",
+			"name": "ALT - alt.ac.uk",
+			"screen_name": "A_L_T",
+			"location": "UK",
+			"description": "We are the Association for Learning Technology (ALT), the leading professional body for Learning Technology in the UK #altc #CMALT",
+			"url": "http://t.co/d7hBNekdUq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/d7hBNekdUq",
+							"expanded_url": "http://www.alt.ac.uk/",
+							"display_url": "alt.ac.uk",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11587,
+			"friends_count": 3511,
+			"listed_count": 573,
+			"created_at": "Tue May 19 21:06:36 +0000 2009",
+			"favourites_count": 5741,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 17505,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/439001186385944576/mrtJJX5d_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/439001186385944576/mrtJJX5d_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/41210876/1552553640",
+			"profile_link_color": "079948",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are now recruiting for @A_L_T 's next Membership Manager and Administration Officer to join the #altc staff team - find out more about working with us https://t.co/zkvEcYZKYG https://t.co/YW1nPUE50n"
+	},
+	{
+		"created_at": "Tue Apr 02 20:46:52 +0000 2019",
+		"id": 1113181021191761900,
+		"id_str": "1113181021191761920",
+		"full_text": "@2michaeltaylor @NicoMiceli @adafruit based near Edinburgh so not sure what the scene is like down there but back in 2017 we did host a conference in Liverpool with a pre-workshop by @hipyliv and @liv_IDEA https://t.co/0XxJd2BI7a",
+		"truncated": false,
+		"display_text_range": [
+			38,
+			229
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "2michaeltaylor",
+					"name": "Michael Taylor",
+					"id": 21192157,
+					"id_str": "21192157",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "NicoMiceli",
+					"name": "Nico Miceli",
+					"id": 280295417,
+					"id_str": "280295417",
+					"indices": [
+						16,
+						27
+					]
+				},
+				{
+					"screen_name": "adafruit",
+					"name": "adafruit industries",
+					"id": 20731304,
+					"id_str": "20731304",
+					"indices": [
+						28,
+						37
+					]
+				},
+				{
+					"screen_name": "hipyliv",
+					"name": "@HiPyLiv",
+					"id": 748789526542241800,
+					"id_str": "748789526542241792",
+					"indices": [
+						183,
+						191
+					]
+				},
+				{
+					"screen_name": "liv_IDEA",
+					"name": "LivIDEA",
+					"id": 819644527929598000,
+					"id_str": "819644527929597953",
+					"indices": [
+						196,
+						205
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/0XxJd2BI7a",
+					"expanded_url": "https://altc.alt.ac.uk/2017/time-slots/pre-conference/",
+					"display_url": "altc.alt.ac.uk/2017/time-slot…",
+					"indices": [
+						206,
+						229
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1113110569811370000,
+		"in_reply_to_status_id_str": "1113110569811369985",
+		"in_reply_to_user_id": 21192157,
+		"in_reply_to_user_id_str": "21192157",
+		"in_reply_to_screen_name": "2michaeltaylor",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Community & Technology Officer @A_L_T | @GoogleDevExpert Apps Script | Interested in Open Education, EdTech, Mashups, Analytics, Data, G Suite \\o/",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8017,
+			"friends_count": 2180,
+			"listed_count": 595,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 5199,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28012,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@2michaeltaylor @NicoMiceli @adafruit based near Edinburgh so not sure what the scene is like down there but back in 2017 we did host a conference in Liverpool with a pre-workshop by @hipyliv and @liv_IDEA https://t.co/0XxJd2BI7a"
+	},
+	{
+		"created_at": "Tue Apr 02 18:11:18 +0000 2019",
+		"id": 1113141874146074600,
+		"id_str": "1113141874146074624",
+		"full_text": "Great Yeoman generator to quickly get your Google App Script project setup for  Google Hangouts Chat (includes clasp/typescript/auto-complete config) by @jeffdaube\n\n#GSuiteDevs #GoogleAppScript\n\n cc @granttimmerman .\n\nhttps://t.co/1i9EjCTTmk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			241
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GSuiteDevs",
+					"indices": [
+						165,
+						176
+					]
+				},
+				{
+					"text": "GoogleAppScript",
+					"indices": [
+						177,
+						193
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jeffdaube",
+					"name": "Jeff Daube",
+					"id": 24368939,
+					"id_str": "24368939",
+					"indices": [
+						153,
+						163
+					]
+				},
+				{
+					"screen_name": "granttimmerman",
+					"name": "Grant Timmerman🦁",
+					"id": 184626074,
+					"id_str": "184626074",
+					"indices": [
+						199,
+						214
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1i9EjCTTmk",
+					"expanded_url": "https://github.com/daubejb/gasbot",
+					"display_url": "github.com/daubejb/gasbot",
+					"indices": [
+						218,
+						241
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Community & Technology Officer @A_L_T | @GoogleDevExpert Apps Script | Interested in Open Education, EdTech, Mashups, Analytics, Data, G Suite \\o/",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8017,
+			"friends_count": 2180,
+			"listed_count": 595,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 5199,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28012,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great Yeoman generator to quickly get your Google App Script project setup for  Google Hangouts Chat (includes clasp/typescript/auto-complete config) by @jeffdaube\n\n#GSuiteDevs #GoogleAppScript\n\n cc @granttimmerman .\n\nhttps://t.co/1i9EjCTTmk"
+	},
+	{
 		"created_at": "Mon Apr 01 19:01:08 +0000 2019",
 		"id": 1112792028587409400,
 		"id_str": "1112792028587409408",
