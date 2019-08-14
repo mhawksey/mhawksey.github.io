994eba01f2585e3be2d96c6ccf71b79192376407
mhawksey <m.hawksey@gmail.com>
2019-08-14 02:10:54 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28739,
-	"created_at": "Tue Aug 13 01:10:51 +0000 2019",
+	"tweets": 28740,
+	"created_at": "Wed Aug 14 01:10:52 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_08.js
+++ tweets/data/js/tweets/2019_08.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 13 17:27:49 +0000 2019",
+		"id": 1161314577478508500,
+		"id_str": "1161328517948346369",
+		"full_text": "Automate humdrum workflows!\n \nIntroducing ✨G Suite Solutions Gallery✨. Automatically populate expense reports with a simple script, or use a custom bot to auto-update work schedules, and more.\n \nLearn how here → https://t.co/HoCV4F0KTk https://t.co/OJs1bSnuZe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HoCV4F0KTk",
+					"expanded_url": "https://goo.gle/33yADvi",
+					"display_url": "goo.gle/33yADvi",
+					"indices": [
+						212,
+						235
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1161314575490461700,
+					"id_str": "1161314575490461696",
+					"indices": [
+						236,
+						259
+					],
+					"media_url": "http://pbs.twimg.com/media/EB3RdBiXsAAwDNP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EB3RdBiXsAAwDNP.jpg",
+					"url": "https://t.co/OJs1bSnuZe",
+					"display_url": "pic.twitter.com/OJs1bSnuZe",
+					"expanded_url": "https://twitter.com/GSuiteDevs/status/1161314577478508544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 555,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 555,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1161314575490461700,
+					"id_str": "1161314575490461696",
+					"indices": [
+						236,
+						259
+					],
+					"media_url": "http://pbs.twimg.com/media/EB3RdBiXsAAwDNP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EB3RdBiXsAAwDNP.jpg",
+					"url": "https://t.co/OJs1bSnuZe",
+					"display_url": "pic.twitter.com/OJs1bSnuZe",
+					"expanded_url": "https://twitter.com/GSuiteDevs/status/1161314577478508544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 555,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 555,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.sprinklr.com\" rel=\"nofollow\">Sprinklr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 111197966,
+			"id_str": "111197966",
+			"name": "G Suite Developers",
+			"screen_name": "GSuiteDevs",
+			"location": "",
+			"description": "Official G Suite Developer feed.\n\nSubscribe to our monthly newsletter: \nhttps://t.co/vjpWf7hSnk",
+			"url": "https://t.co/GkUhgOjCaA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GkUhgOjCaA",
+							"expanded_url": "http://developers.google.com/gsuite",
+							"display_url": "developers.google.com/gsuite",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vjpWf7hSnk",
+							"expanded_url": "https://developers.google.com/gsuite/newsletters/#subscribe",
+							"display_url": "developers.google.com/gsuite/newslet…",
+							"indices": [
+								72,
+								95
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 330222,
+			"friends_count": 36,
+			"listed_count": 2754,
+			"created_at": "Thu Feb 04 03:22:30 +0000 2010",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 640,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/781544329416351744/1Phh0h2q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/781544329416351744/1Phh0h2q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/111197966/1558458191",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Automate humdrum workflows!\n \nIntroducing ✨G Suite Solutions Gallery✨. Automatically populate expense reports with a simple script, or use a custom bot to auto-update work schedules, and more.\n \nLearn how here → https://t.co/HoCV4F0KTk https://t.co/OJs1bSnuZe"
+	},
+	{
 		"created_at": "Mon Aug 12 18:58:20 +0000 2019",
 		"id": 1160988906399178800,
 		"id_str": "1160988906399178752",
