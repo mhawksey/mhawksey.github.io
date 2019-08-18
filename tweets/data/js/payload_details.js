e698f7bb1f8c1b03bc882a836a7fa89b00654c6b
mhawksey <m.hawksey@gmail.com>
2019-08-18 02:10:52 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28743,
-	"created_at": "Sat Aug 17 01:10:51 +0000 2019",
+	"tweets": 28744,
+	"created_at": "Sun Aug 18 01:10:51 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_08.js
+++ tweets/data/js/tweets/2019_08.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 17 11:39:15 +0000 2019",
+		"id": 1162690350353408000,
+		"id_str": "1162690350353408000",
+		"full_text": "@kerrg The Google Books project actually established case law in terms of \"fair use\" data mining of copyrighted text  https://t.co/BwhNuZBvSE",
+		"truncated": false,
+		"display_text_range": [
+			7,
+			141
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kerrg",
+					"name": "Kerr Gardiner",
+					"id": 6348602,
+					"id_str": "6348602",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BwhNuZBvSE",
+					"expanded_url": "https://en.wikipedia.org/wiki/Fair_use#Text_and_data_mining",
+					"display_url": "en.wikipedia.org/wiki/Fair_use#…",
+					"indices": [
+						118,
+						141
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1162673253678231600,
+		"in_reply_to_status_id_str": "1162673253678231552",
+		"in_reply_to_user_id": 6348602,
+		"in_reply_to_user_id_str": "6348602",
+		"in_reply_to_screen_name": "kerrg",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8246,
+			"friends_count": 2347,
+			"listed_count": 614,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 5823,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28608,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@kerrg The Google Books project actually established case law in terms of \"fair use\" data mining of copyrighted text  https://t.co/BwhNuZBvSE"
+	},
+	{
 		"created_at": "Fri Aug 16 16:41:25 +0000 2019",
 		"id": 1162404002581160000,
 		"id_str": "1162404002581159941",
