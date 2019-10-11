faba6412ee59d78cabefd351879c8ef9938bf8b6
mhawksey <m.hawksey@gmail.com>
2019-10-11 02:10:52 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28931,
-	"created_at": "Thu Oct 10 01:10:51 +0000 2019",
+	"tweets": 28932,
+	"created_at": "Fri Oct 11 01:10:51 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_10.js
+++ tweets/data/js/tweets/2019_10.js
@@ -1,6 +1,353 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 10 08:24:21 +0000 2019",
+		"id": 1182210246879699000,
+		"id_str": "1182210246879698944",
+		"full_text": "This is a great read from @techstreams ... not just for the #GoogleAppsScript but to learn about Benford's Law [@benlcollins think you'll like if not already seen] #GSuiteDevs https://t.co/2WQm2byN5B",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						60,
+						77
+					]
+				},
+				{
+					"text": "GSuiteDevs",
+					"indices": [
+						164,
+						175
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "techstreams",
+					"name": "Laura Taylor",
+					"id": 14328802,
+					"id_str": "14328802",
+					"indices": [
+						26,
+						38
+					]
+				},
+				{
+					"screen_name": "benlcollins",
+					"name": "Ben Collins",
+					"id": 19338407,
+					"id_str": "19338407",
+					"indices": [
+						112,
+						124
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2WQm2byN5B",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1181866905209995264",
+					"display_url": "twitter.com/AppsScriptInfo…",
+					"indices": [
+						176,
+						199
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8308,
+			"friends_count": 2395,
+			"listed_count": 618,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 6010,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28830,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1181866905209995300,
+		"quoted_status_id_str": "1181866905209995264",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2WQm2byN5B",
+			"expanded": "https://twitter.com/AppsScriptInfo/status/1181866905209995264",
+			"display": "twitter.com/AppsScriptInfo…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Oct 09 09:40:02 +0000 2019",
+			"id": 1181866905209995300,
+			"id_str": "1181866905209995264",
+			"full_text": "Using Apps Script, a Google Sheets Macro and Benford’s Law to Detect Potential Fraud – Laura Taylor #GSuiteDevs #GoogleAppsScript https://t.co/9qcXuvAHEN https://t.co/9bLX6LSPKU",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				153
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GSuiteDevs",
+						"indices": [
+							100,
+							111
+						]
+					},
+					{
+						"text": "GoogleAppsScript",
+						"indices": [
+							112,
+							129
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/9qcXuvAHEN",
+						"expanded_url": "https://pulse.appsscript.info/p/2019/10/using-apps-script-a-google-sheets-macro-and-benfords-law-to-detect-potential-fraud-laura-taylor/",
+						"display_url": "pulse.appsscript.info/p/2019/10/usin…",
+						"indices": [
+							130,
+							153
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1181866903423221800,
+						"id_str": "1181866903423221760",
+						"indices": [
+							154,
+							177
+						],
+						"media_url": "http://pbs.twimg.com/media/EGbVr9ZVAAAXEXu.png",
+						"media_url_https": "https://pbs.twimg.com/media/EGbVr9ZVAAAXEXu.png",
+						"url": "https://t.co/9bLX6LSPKU",
+						"display_url": "pic.twitter.com/9bLX6LSPKU",
+						"expanded_url": "https://twitter.com/AppsScriptInfo/status/1181866905209995264/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 353,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 706,
+								"h": 366,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 706,
+								"h": 366,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1181866903423221800,
+						"id_str": "1181866903423221760",
+						"indices": [
+							154,
+							177
+						],
+						"media_url": "http://pbs.twimg.com/media/EGbVr9ZVAAAXEXu.png",
+						"media_url_https": "https://pbs.twimg.com/media/EGbVr9ZVAAAXEXu.png",
+						"url": "https://t.co/9bLX6LSPKU",
+						"display_url": "pic.twitter.com/9bLX6LSPKU",
+						"expanded_url": "https://twitter.com/AppsScriptInfo/status/1181866905209995264/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 353,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 706,
+								"h": 366,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 706,
+								"h": 366,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 322526439,
+				"id_str": "322526439",
+				"name": "AppsScriptInfo (supporting #GSuiteDevs)",
+				"screen_name": "AppsScriptInfo",
+				"location": "World",
+				"description": "A Google Apps Script Community account to support #GSuiteDevs. Curated by @mhawksey",
+				"url": "https://t.co/1OwN2T5Za9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/1OwN2T5Za9",
+								"expanded_url": "http://tu.appsscript.info",
+								"display_url": "tu.appsscript.info",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 234,
+				"friends_count": 368,
+				"listed_count": 11,
+				"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+				"favourites_count": 69,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1227,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+				"profile_link_color": "4286F4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a great read from @techstreams ... not just for the #GoogleAppsScript but to learn about Benford's Law [@benlcollins think you'll like if not already seen] #GSuiteDevs https://t.co/2WQm2byN5B"
+	},
+	{
 		"created_at": "Wed Oct 09 20:57:37 +0000 2019",
 		"id": 1182037421602607000,
 		"id_str": "1182037421602607105",
@@ -132,7 +479,7 @@ Grailbird.data.tweets_2019_10 =
 				},
 				{
 					"screen_name": "SFarley_Charlie",
-					"name": "Charlie 🐿️ 🏳️‍🌈",
+					"name": "Charlie 🐿️ 🏳️🌈",
 					"id": 83184867,
 					"id_str": "83184867",
 					"indices": [
