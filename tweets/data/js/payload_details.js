459e863e52c7ba0d1e374cb54c948d4a619142e5
mhawksey <m.hawksey@gmail.com>
2019-11-01 18:10:53 -0700

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28979,
-	"created_at": "Fri Nov 01 01:10:52 +0000 2019",
+	"tweets": 28981,
+	"created_at": "Sat Nov 02 01:10:52 +0000 2019",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2019_11.js
@@ -0,0 +1,315 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 21:29:45 +0000 2019",
+		"id": 1190380431964807200,
+		"id_str": "1190380431964807168",
+		"full_text": "@benlcollins @GoogleDevExpert @GDGglasgow @GDGMiltonKeynes @gdglondon ... planning something a little more streamlined compared to my last talk ;) https://t.co/RLfnhkk8oO",
+		"truncated": false,
+		"display_text_range": [
+			70,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "benlcollins",
+					"name": "Ben Collins",
+					"id": 19338407,
+					"id_str": "19338407",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "GoogleDevExpert",
+					"name": "Google Developers Experts",
+					"id": 3003834689,
+					"id_str": "3003834689",
+					"indices": [
+						13,
+						29
+					]
+				},
+				{
+					"screen_name": "GDGglasgow",
+					"name": "GDG Glasgow",
+					"id": 1047034492748214300,
+					"id_str": "1047034492748214272",
+					"indices": [
+						30,
+						41
+					]
+				},
+				{
+					"screen_name": "GDGMiltonKeynes",
+					"name": "GDG Milton Keynes",
+					"id": 1149597394801004500,
+					"id_str": "1149597394801004544",
+					"indices": [
+						42,
+						58
+					]
+				},
+				{
+					"screen_name": "gdglondon",
+					"name": "GDG London",
+					"id": 53442117,
+					"id_str": "53442117",
+					"indices": [
+						59,
+						69
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RLfnhkk8oO",
+					"expanded_url": "https://mashe.hawksey.info/2019/07/domains19-the-art-of-surveillance-and-video-production/",
+					"display_url": "mashe.hawksey.info/2019/07/domain…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1190355239586717700,
+		"in_reply_to_status_id_str": "1190355239586717698",
+		"in_reply_to_user_id": 19338407,
+		"in_reply_to_user_id_str": "19338407",
+		"in_reply_to_screen_name": "benlcollins",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8349,
+			"friends_count": 2419,
+			"listed_count": 618,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 6071,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28892,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@benlcollins @GoogleDevExpert @GDGglasgow @GDGMiltonKeynes @gdglondon ... planning something a little more streamlined compared to my last talk ;) https://t.co/RLfnhkk8oO"
+	},
+	{
+		"created_at": "Fri Nov 01 19:11:56 +0000 2019",
+		"id": 1190345746761080800,
+		"id_str": "1190345746761080832",
+		"full_text": "My #DevFest19/@GoogleDevExpert tour starts tomorrow @GDGglasgow talking about Google Analytics of Things, following Saturday @GDGMiltonKeynes for GA talk plus Conversational Interfaces in #GSuite Editors and finally 16 Nov @gdglondon taking Conversational Interfaces",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DevFest19",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"text": "GSuite",
+					"indices": [
+						188,
+						195
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GoogleDevExpert",
+					"name": "Google Developers Experts",
+					"id": 3003834689,
+					"id_str": "3003834689",
+					"indices": [
+						14,
+						30
+					]
+				},
+				{
+					"screen_name": "GDGglasgow",
+					"name": "GDG Glasgow",
+					"id": 1047034492748214300,
+					"id_str": "1047034492748214272",
+					"indices": [
+						52,
+						63
+					]
+				},
+				{
+					"screen_name": "GDGMiltonKeynes",
+					"name": "GDG Milton Keynes",
+					"id": 1149597394801004500,
+					"id_str": "1149597394801004544",
+					"indices": [
+						125,
+						141
+					]
+				},
+				{
+					"screen_name": "gdglondon",
+					"name": "GDG London",
+					"id": 53442117,
+					"id_str": "53442117",
+					"indices": [
+						223,
+						233
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8349,
+			"friends_count": 2419,
+			"listed_count": 618,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 6071,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28892,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "My #DevFest19/@GoogleDevExpert tour starts tomorrow @GDGglasgow talking about Google Analytics of Things, following Saturday @GDGMiltonKeynes for GA talk plus Conversational Interfaces in #GSuite Editors and finally 16 Nov @gdglondon taking Conversational Interfaces"
+	}
+]
