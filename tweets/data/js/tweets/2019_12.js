41addcdf91df76f874af9b80c8eb55d0043e7a6a
mhawksey <m.hawksey@gmail.com>
2019-12-03 01:10:54 +0000

    Added 2 tweets

---
+++ tweets/data/js/tweets/2019_12.js
@@ -0,0 +1,624 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Mon Dec 02 12:41:12 +0000 2019",
+		"id": 1201481442066346000,
+		"id_str": "1201481442066345984",
+		"full_text": "You can use 'Find and replace' in Google Sheets to update tick boxes. To untick all boxes you can find TRUE and replace with FALSE  \n\n#GoogleSheets #SheetsTip #GoogleEDU #GSuite https://t.co/h0Ux534QCE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleSheets",
+					"indices": [
+						134,
+						147
+					]
+				},
+				{
+					"text": "SheetsTip",
+					"indices": [
+						148,
+						158
+					]
+				},
+				{
+					"text": "GoogleEDU",
+					"indices": [
+						159,
+						169
+					]
+				},
+				{
+					"text": "GSuite",
+					"indices": [
+						170,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1201480780620451800,
+					"id_str": "1201480780620451841",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/EKyEZ31XUAEGcDg.png",
+					"media_url_https": "https://pbs.twimg.com/media/EKyEZ31XUAEGcDg.png",
+					"url": "https://t.co/h0Ux534QCE",
+					"display_url": "pic.twitter.com/h0Ux534QCE",
+					"expanded_url": "https://twitter.com/mhawksey/status/1201481442066345984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 441,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 775,
+							"h": 503,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 775,
+							"h": 503,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1201480780620451800,
+					"id_str": "1201480780620451841",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/EKyEZ31XUAEGcDg.png",
+					"media_url_https": "https://pbs.twimg.com/media/EKyEZ31XUAEGcDg.png",
+					"url": "https://t.co/h0Ux534QCE",
+					"display_url": "pic.twitter.com/h0Ux534QCE",
+					"expanded_url": "https://twitter.com/mhawksey/status/1201481442066345984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 441,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 775,
+							"h": 503,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 775,
+							"h": 503,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8410,
+			"friends_count": 2455,
+			"listed_count": 618,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 6185,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29038,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "You can use 'Find and replace' in Google Sheets to update tick boxes. To untick all boxes you can find TRUE and replace with FALSE  \n\n#GoogleSheets #SheetsTip #GoogleEDU #GSuite https://t.co/h0Ux534QCE"
+	},
+	{
+		"created_at": "Mon Dec 02 12:10:51 +0000 2019",
+		"id": 1197957635657621500,
+		"id_str": "1201473802519171072",
+		"full_text": "By me: Hacking Google Slides as a voice enabled presentation tool using Google Apps Script https://t.co/ttEpgWVNW0 #GoogleAppsScript #GSuiteDevs #GoogleIE https://t.co/vmDbceXO8b",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						115,
+						132
+					]
+				},
+				{
+					"text": "GSuiteDevs",
+					"indices": [
+						133,
+						144
+					]
+				},
+				{
+					"text": "GoogleIE",
+					"indices": [
+						145,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ttEpgWVNW0",
+					"expanded_url": "https://mashe.hawksey.info/2019/11/hacking-google-slides-as-a-voice-enabled-presentation-tool-using-google-apps-script/",
+					"display_url": "mashe.hawksey.info/2019/11/hackin…",
+					"indices": [
+						91,
+						114
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1197957629500362800,
+					"id_str": "1197957629500362752",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/EKAAHfJWkAA8h8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKAAHfJWkAA8h8w.jpg",
+					"url": "https://t.co/vmDbceXO8b",
+					"display_url": "pic.twitter.com/vmDbceXO8b",
+					"expanded_url": "https://twitter.com/mhawksey/status/1197957635657621505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 328,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1836,
+							"h": 886,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 579,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 315,
+									"y": 183,
+									"h": 22,
+									"w": 22
+								},
+								{
+									"x": 228,
+									"y": 191,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 95,
+									"y": 54,
+									"h": 231,
+									"w": 231
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 853,
+									"y": 496,
+									"h": 62,
+									"w": 62
+								},
+								{
+									"x": 618,
+									"y": 516,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 258,
+									"y": 148,
+									"h": 625,
+									"w": 625
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 557,
+									"y": 324,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 403,
+									"y": 337,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 168,
+									"y": 96,
+									"h": 408,
+									"w": 408
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 853,
+									"y": 496,
+									"h": 62,
+									"w": 62
+								},
+								{
+									"x": 618,
+									"y": 516,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 258,
+									"y": 148,
+									"h": 625,
+									"w": 625
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197957629500362800,
+					"id_str": "1197957629500362752",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/EKAAHfJWkAA8h8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKAAHfJWkAA8h8w.jpg",
+					"url": "https://t.co/vmDbceXO8b",
+					"display_url": "pic.twitter.com/vmDbceXO8b",
+					"expanded_url": "https://twitter.com/mhawksey/status/1197957635657621505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 328,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1836,
+							"h": 886,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 579,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 315,
+									"y": 183,
+									"h": 22,
+									"w": 22
+								},
+								{
+									"x": 228,
+									"y": 191,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 95,
+									"y": 54,
+									"h": 231,
+									"w": 231
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 853,
+									"y": 496,
+									"h": 62,
+									"w": 62
+								},
+								{
+									"x": 618,
+									"y": 516,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 258,
+									"y": 148,
+									"h": 625,
+									"w": 625
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 557,
+									"y": 324,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 403,
+									"y": 337,
+									"h": 50,
+									"w": 50
+								},
+								{
+									"x": 168,
+									"y": 96,
+									"h": 408,
+									"w": 408
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 853,
+									"y": 496,
+									"h": 62,
+									"w": 62
+								},
+								{
+									"x": 618,
+									"y": 516,
+									"h": 77,
+									"w": 77
+								},
+								{
+									"x": 258,
+									"y": 148,
+									"h": 625,
+									"w": 625
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8410,
+			"friends_count": 2455,
+			"listed_count": 618,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 6185,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29038,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 24,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "By me: Hacking Google Slides as a voice enabled presentation tool using Google Apps Script https://t.co/ttEpgWVNW0 #GoogleAppsScript #GSuiteDevs #GoogleIE https://t.co/vmDbceXO8b"
+	}
+]
