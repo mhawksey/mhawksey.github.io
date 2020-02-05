b94c647ce87b79ac2da051634385406d4a012683
mhawksey <m.hawksey@gmail.com>
2020-02-05 01:10:53 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29346,
-	"created_at": "Tue Feb 04 01:10:52 +0000 2020",
+	"tweets": 29347,
+	"created_at": "Wed Feb 05 01:10:51 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_02.js
+++ tweets/data/js/tweets/2020_02.js
@@ -1,6 +1,136 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 04 09:47:50 +0000 2020",
+		"id": 1223639947502026800,
+		"id_str": "1224630636218109952",
+		"full_text": "h5p as a quiz question type in #Moodle would be  a super addition  https://t.co/yTo8j65fPi please vote and retweet too #heie #altc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Moodle",
+					"indices": [
+						31,
+						38
+					]
+				},
+				{
+					"text": "heie",
+					"indices": [
+						119,
+						124
+					]
+				},
+				{
+					"text": "altc",
+					"indices": [
+						125,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yTo8j65fPi",
+					"expanded_url": "https://h5p.org/node/192640#comment-form",
+					"display_url": "h5p.org/node/192640#co…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 80048517,
+			"id_str": "80048517",
+			"name": "Mark Glynn",
+			"screen_name": "glynnmark",
+			"location": "Dublin, Ireland",
+			"description": "Head of Teaching Enhancement Unit in Dublin City University. I manage both organisational and pedagogical projects to improve the learner experience.",
+			"url": "http://t.co/UbK3OkFPfx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/UbK3OkFPfx",
+							"expanded_url": "http://enhancingteaching.com/",
+							"display_url": "enhancingteaching.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3338,
+			"friends_count": 1912,
+			"listed_count": 161,
+			"created_at": "Mon Oct 05 15:18:27 +0000 2009",
+			"favourites_count": 3783,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/566153622505803776/l6v-tm3m_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/566153622505803776/l6v-tm3m_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/80048517/1443197488",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "h5p as a quiz question type in #Moodle would be  a super addition  https://t.co/yTo8j65fPi please vote and retweet too #heie #altc"
+	},
+	{
 		"created_at": "Mon Feb 03 11:31:43 +0000 2020",
 		"id": 1224294388823666700,
 		"id_str": "1224294388823666688",
