d907830dc5504d7697311687fc06795793c81ea5
mhawksey <m.hawksey@gmail.com>
2020-03-01 01:10:55 +0000

    Added 1 tweets

--- tweets/data/js/tweets/2020_02.js
+++ tweets/data/js/tweets/2020_02.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 29 08:13:22 +0000 2020",
+		"id": 1233056481546842000,
+		"id_str": "1233666556095275008",
+		"full_text": "Wow, we just hit 3,000 registrations for #SheetsCon2020! 🤯🎉\n\nSee you at the inaugural edition of this free, online conference for #GoogleSheets 🙌\n\nThank you all!\n\nGrab your own ticket here: https://t.co/5WkU24b63M https://t.co/66EbW62tgZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			213
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SheetsCon2020",
+					"indices": [
+						41,
+						55
+					]
+				},
+				{
+					"text": "GoogleSheets",
+					"indices": [
+						130,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5WkU24b63M",
+					"expanded_url": "https://sheetscon.com/",
+					"display_url": "sheetscon.com",
+					"indices": [
+						190,
+						213
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1233056292572422100,
+					"id_str": "1233056292572422146",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/ERyyKkLW4AIa-Da.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERyyKkLW4AIa-Da.jpg",
+					"url": "https://t.co/66EbW62tgZ",
+					"display_url": "pic.twitter.com/66EbW62tgZ",
+					"expanded_url": "https://twitter.com/benlcollins/status/1233056481546842114/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 374,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1029,
+							"h": 566,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1029,
+							"h": 566,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1233056292572422100,
+					"id_str": "1233056292572422146",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/ERyyKkLW4AIa-Da.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERyyKkLW4AIa-Da.jpg",
+					"url": "https://t.co/66EbW62tgZ",
+					"display_url": "pic.twitter.com/66EbW62tgZ",
+					"expanded_url": "https://twitter.com/benlcollins/status/1233056481546842114/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 374,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1029,
+							"h": 566,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1029,
+							"h": 566,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19338407,
+			"id_str": "19338407",
+			"name": "Ben Collins",
+			"screen_name": "benlcollins",
+			"location": "Harpers Ferry, WV",
+			"description": "Teach https://t.co/UXcar5Uxsr, creator of https://t.co/IMvxbynrzD, Google Developer Expert for GSuite. Sign up for Sheets tips: https://t.co/SV7xtnBj1G",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXcar5Uxsr",
+							"expanded_url": "http://courses.benlcollins.com",
+							"display_url": "courses.benlcollins.com",
+							"indices": [
+								6,
+								29
+							]
+						},
+						{
+							"url": "https://t.co/IMvxbynrzD",
+							"expanded_url": "http://sheetscon.com",
+							"display_url": "sheetscon.com",
+							"indices": [
+								42,
+								65
+							]
+						},
+						{
+							"url": "https://t.co/SV7xtnBj1G",
+							"expanded_url": "http://benlcollins.com/google-sheets-tips",
+							"display_url": "benlcollins.com/google-sheets-…",
+							"indices": [
+								128,
+								151
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4223,
+			"friends_count": 536,
+			"listed_count": 269,
+			"created_at": "Thu Jan 22 13:43:22 +0000 2009",
+			"favourites_count": 8263,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8572,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "CFCFCF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/966495721598935040/y1KUcaEN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/966495721598935040/y1KUcaEN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19338407/1582908631",
+			"profile_link_color": "619DE6",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "ECFFD6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 24,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wow, we just hit 3,000 registrations for #SheetsCon2020! 🤯🎉\n\nSee you at the inaugural edition of this free, online conference for #GoogleSheets 🙌\n\nThank you all!\n\nGrab your own ticket here: https://t.co/5WkU24b63M https://t.co/66EbW62tgZ"
+	},
+	{
 		"created_at": "Fri Feb 28 16:01:08 +0000 2020",
 		"id": 1233421888732483600,
 		"id_str": "1233421888732483585",
