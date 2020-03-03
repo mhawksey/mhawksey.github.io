c0e27ab177cadd7898ff99431330dbd1bc5303b7
mhawksey <m.hawksey@gmail.com>
2020-03-03 01:10:53 +0000

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29420,
-	"created_at": "Sun Mar 01 01:10:53 +0000 2020",
+	"tweets": 29422,
+	"created_at": "Tue Mar 03 01:10:52 +0000 2020",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2020_03.js
@@ -0,0 +1,249 @@
+Grailbird.data.tweets_2020_03 = 
+[
+	{
+		"created_at": "Mon Mar 02 20:34:05 +0000 2020",
+		"id": 1234563681725358000,
+		"id_str": "1234577740818460672",
+		"full_text": "OER20 preview--&gt; THis webinar is not to miss! https://t.co/bwAwioV25B via @A_L_T #oer20",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "oer20",
+					"indices": [
+						84,
+						90
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "A_L_T",
+					"name": "ALT - alt.ac.uk",
+					"id": 41210876,
+					"id_str": "41210876",
+					"indices": [
+						77,
+						83
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/bwAwioV25B",
+					"expanded_url": "https://altc.alt.ac.uk/oesig/2020/02/25/oer20-preview/",
+					"display_url": "altc.alt.ac.uk/oesig/2020/02/…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19557144,
+			"id_str": "19557144",
+			"name": "✿Caroline Kühn H✿",
+			"screen_name": "carolak",
+			"location": "Bath, United Kingdom",
+			"description": "Open Educator-Researcher| Senior Lecturer: Ed+Tech @BathSpaUni | #GO_GN |PhD candidate looking @ agency in Open Learning Spaces| students' perspective| (∞.∞)|🐢",
+			"url": "https://t.co/g1tI9r4lpP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/g1tI9r4lpP",
+							"expanded_url": "https://carolinekuhn.net/",
+							"display_url": "carolinekuhn.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1014,
+			"friends_count": 1394,
+			"listed_count": 85,
+			"created_at": "Mon Jan 26 20:57:04 +0000 2009",
+			"favourites_count": 5521,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6240,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0D888C",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875128702190985216/WnDMyfED_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875128702190985216/WnDMyfED_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19557144/1398620546",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "OER20 preview--&gt; THis webinar is not to miss! https://t.co/bwAwioV25B via @A_L_T #oer20"
+	},
+	{
+		"created_at": "Mon Mar 02 19:42:54 +0000 2020",
+		"id": 1234564861788643300,
+		"id_str": "1234564861788643330",
+		"full_text": "7 March Edinburgh Hoppers are having their annual celebration of Women and other gender minorities in Tech as part of #IWD2020 https://t.co/SOt5IqtnGe #FemEdTech",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			161
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IWD2020",
+					"indices": [
+						118,
+						126
+					]
+				},
+				{
+					"text": "FemEdTech",
+					"indices": [
+						151,
+						161
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SOt5IqtnGe",
+					"expanded_url": "https://edinburgh-hoppers.github.io/website/iwd20",
+					"display_url": "edinburgh-hoppers.github.io/website/iwd20",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Com & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8570,
+			"friends_count": 2515,
+			"listed_count": 620,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 6442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29423,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "7 March Edinburgh Hoppers are having their annual celebration of Women and other gender minorities in Tech as part of #IWD2020 https://t.co/SOt5IqtnGe #FemEdTech"
+	}
+]
