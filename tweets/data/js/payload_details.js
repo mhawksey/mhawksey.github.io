77060f9f5578487242b341120f566516be29e262
mhawksey <m.hawksey@gmail.com>
2020-04-13 02:10:53 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29560,
-	"created_at": "Sun Apr 12 01:10:52 +0000 2020",
+	"tweets": 29561,
+	"created_at": "Mon Apr 13 01:10:52 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_04.js
+++ tweets/data/js/tweets/2020_04.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sun Apr 12 14:12:58 +0000 2020",
+		"id": 1245752172718903300,
+		"id_str": "1249339731382599683",
+		"full_text": "💡 Help your peers learn and connect.\n\nBecome a #DeveloperStudentClubs Lead to help train student developers around campus and work with your local communities to solve real-life problems. \n\n✉️ Forms now open → https://t.co/WQulzvpErW https://t.co/nydLrDzQyv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			233
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DeveloperStudentClubs",
+					"indices": [
+						47,
+						69
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WQulzvpErW",
+					"expanded_url": "https://goo.gle/dsc-leads",
+					"display_url": "goo.gle/dsc-leads",
+					"indices": [
+						210,
+						233
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1245751880929513500,
+					"id_str": "1245751880929513474",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245751880929513474/pu/img/IKfvDdBF-DJqG8aD.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245751880929513474/pu/img/IKfvDdBF-DJqG8aD.jpg",
+					"url": "https://t.co/nydLrDzQyv",
+					"display_url": "pic.twitter.com/nydLrDzQyv",
+					"expanded_url": "https://twitter.com/googledevs/status/1245752172718903296/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245751880929513500,
+					"id_str": "1245751880929513474",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245751880929513474/pu/img/IKfvDdBF-DJqG8aD.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245751880929513474/pu/img/IKfvDdBF-DJqG8aD.jpg",
+					"url": "https://t.co/nydLrDzQyv",
+					"display_url": "pic.twitter.com/nydLrDzQyv",
+					"expanded_url": "https://twitter.com/googledevs/status/1245752172718903296/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 45963,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245751880929513474/pu/vid/480x270/Y-l7ywGhrSexX2Xb.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245751880929513474/pu/vid/640x360/9aUWF9hyyMAH8tRy.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1245751880929513474/pu/pl/RupKlmQuI4wTMqRv.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245751880929513474/pu/vid/1280x720/Scl1HrvnbUpgC9z6.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 50090898,
+			"id_str": "50090898",
+			"name": "Google Developers",
+			"screen_name": "googledevs",
+			"location": "",
+			"description": "Now serving 2 Million subscribers on YouTube. Thanks for tuning in! https://t.co/PecU8JeURq",
+			"url": "http://t.co/aBJokfscjb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/aBJokfscjb",
+							"expanded_url": "http://developers.google.com",
+							"display_url": "developers.google.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PecU8JeURq",
+							"expanded_url": "http://g.co/dev/youtube",
+							"display_url": "g.co/dev/youtube",
+							"indices": [
+								68,
+								91
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2302398,
+			"friends_count": 265,
+			"listed_count": 16982,
+			"created_at": "Tue Jun 23 20:25:29 +0000 2009",
+			"favourites_count": 1235,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6042,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1111670607026884609/ma2jqJKH_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1111670607026884609/ma2jqJKH_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/50090898/1581287874",
+			"profile_link_color": "4585F1",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E6E8EB",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 189,
+		"favorite_count": 454,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "💡 Help your peers learn and connect.\n\nBecome a #DeveloperStudentClubs Lead to help train student developers around campus and work with your local communities to solve real-life problems. \n\n✉️ Forms now open → https://t.co/WQulzvpErW https://t.co/nydLrDzQyv"
+	},
+	{
 		"created_at": "Sat Apr 11 15:25:43 +0000 2020",
 		"id": 1248924705035337700,
 		"id_str": "1248995652006227970",
