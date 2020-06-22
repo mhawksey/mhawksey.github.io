b50c0a648d50bb87c91290144b662f21a9c515ff
mhawksey <m.hawksey@gmail.com>
2020-06-22 02:10:53 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29767,
-	"created_at": "Sun Jun 21 01:10:52 +0000 2020",
+	"tweets": 29768,
+	"created_at": "Mon Jun 22 01:10:52 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_06.js
+++ tweets/data/js/tweets/2020_06.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 21 12:03:38 +0000 2020",
+		"id": 1274665754957197300,
+		"id_str": "1274674337291829249",
+		"full_text": "Getting started with the Google Vision API from G Suite | Google Cloud Blog by @cschalk (contributed by @mhawksey) #GSuiteDevs #GoogleAppsScript https://t.co/t2EZNGPyy7 https://t.co/zjzATlr1sj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GSuiteDevs",
+					"indices": [
+						115,
+						126
+					]
+				},
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						127,
+						144
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cschalk",
+					"name": "Chris Schalk",
+					"id": 14138023,
+					"id_str": "14138023",
+					"indices": [
+						79,
+						87
+					]
+				},
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey #altc",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						104,
+						113
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/t2EZNGPyy7",
+					"expanded_url": "https://pulse.appsscript.info/p/2020/06/getting-started-with-the-google-vision-api-from-g-suite-google-cloud-blog/",
+					"display_url": "pulse.appsscript.info/p/2020/06/gett…",
+					"indices": [
+						145,
+						168
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1274665753229189000,
+					"id_str": "1274665753229189121",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/EbCFv8SU4AE3WFR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbCFv8SU4AE3WFR.jpg",
+					"url": "https://t.co/zjzATlr1sj",
+					"display_url": "pic.twitter.com/zjzATlr1sj",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1274665754957197312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 283,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 625,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1274665753229189000,
+					"id_str": "1274665753229189121",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/EbCFv8SU4AE3WFR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbCFv8SU4AE3WFR.jpg",
+					"url": "https://t.co/zjzATlr1sj",
+					"display_url": "pic.twitter.com/zjzATlr1sj",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1274665754957197312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 283,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 625,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 322526439,
+			"id_str": "322526439",
+			"name": "AppsScriptInfo (supporting #GSuiteDevs)",
+			"screen_name": "AppsScriptInfo",
+			"location": "World",
+			"description": "A Google Apps Script Community account to support #GSuiteDevs. Curated by @mhawksey",
+			"url": "https://t.co/pEqPAHFH4F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEqPAHFH4F",
+							"expanded_url": "https://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 719,
+			"friends_count": 856,
+			"listed_count": 21,
+			"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+			"favourites_count": 495,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1771,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+			"profile_link_color": "4286F4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Getting started with the Google Vision API from G Suite | Google Cloud Blog by @cschalk (contributed by @mhawksey) #GSuiteDevs #GoogleAppsScript https://t.co/t2EZNGPyy7 https://t.co/zjzATlr1sj"
+	},
+	{
 		"created_at": "Sat Jun 20 16:02:34 +0000 2020",
 		"id": 1274372078402142200,
 		"id_str": "1274372078402142208",
