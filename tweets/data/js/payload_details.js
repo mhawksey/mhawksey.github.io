319bc7b40ac091e860f8a47b96cb48bde74300ae
mhawksey <m.hawksey@gmail.com>
2020-07-07 02:10:53 +0100

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29795,
-	"created_at": "Sat Jul 04 01:10:52 +0000 2020",
+	"tweets": 29797,
+	"created_at": "Tue Jul 07 01:10:52 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_07.js
+++ tweets/data/js/tweets/2020_07.js
@@ -1,6 +1,436 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 06 21:33:34 +0000 2020",
+		"id": 1280239860402405400,
+		"id_str": "1280253581900341251",
+		"full_text": "Calculating moving averages in Google Sheets with Google Apps Script (and tips on writing custom functions) by @pfelipm (contributed by @mhawksey) #GSuiteDevs #GoogleAppsScript https://t.co/wbBxf7ZBhD https://t.co/yf0zRTw9Zf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			200
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GSuiteDevs",
+					"indices": [
+						147,
+						158
+					]
+				},
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						159,
+						176
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pfelipm",
+					"name": "Pablo Felip",
+					"id": 127973484,
+					"id_str": "127973484",
+					"indices": [
+						111,
+						119
+					]
+				},
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey #altc",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						136,
+						145
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wbBxf7ZBhD",
+					"expanded_url": "https://pulse.appsscript.info/p/2020/07/calculating-moving-averages-in-google-sheets-with-google-apps-script-and-tips-on-writing-custom-functions/",
+					"display_url": "pulse.appsscript.info/p/2020/07/calc…",
+					"indices": [
+						177,
+						200
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1280239858015801300,
+					"id_str": "1280239858015801344",
+					"indices": [
+						201,
+						224
+					],
+					"media_url": "http://pbs.twimg.com/media/EcRTXixUEAA0YDi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcRTXixUEAA0YDi.jpg",
+					"url": "https://t.co/yf0zRTw9Zf",
+					"display_url": "pic.twitter.com/yf0zRTw9Zf",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1280239860402405377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 893,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 296,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 523,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1280239858015801300,
+					"id_str": "1280239858015801344",
+					"indices": [
+						201,
+						224
+					],
+					"media_url": "http://pbs.twimg.com/media/EcRTXixUEAA0YDi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcRTXixUEAA0YDi.jpg",
+					"url": "https://t.co/yf0zRTw9Zf",
+					"display_url": "pic.twitter.com/yf0zRTw9Zf",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1280239860402405377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 893,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 296,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 523,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 322526439,
+			"id_str": "322526439",
+			"name": "AppsScriptInfo (supporting #GSuiteDevs)",
+			"screen_name": "AppsScriptInfo",
+			"location": "World",
+			"description": "A Google Apps Script Community account to support #GSuiteDevs. Curated by @mhawksey",
+			"url": "https://t.co/pEqPAHFH4F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEqPAHFH4F",
+							"expanded_url": "https://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 744,
+			"friends_count": 878,
+			"listed_count": 21,
+			"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+			"favourites_count": 499,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1789,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+			"profile_link_color": "4286F4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Calculating moving averages in Google Sheets with Google Apps Script (and tips on writing custom functions) by @pfelipm (contributed by @mhawksey) #GSuiteDevs #GoogleAppsScript https://t.co/wbBxf7ZBhD https://t.co/yf0zRTw9Zf"
+	},
+	{
+		"created_at": "Mon Jul 06 17:41:28 +0000 2020",
+		"id": 1280108493064867800,
+		"id_str": "1280195171175600128",
+		"full_text": "Glad to see more #edtech folk joining @A_L_T each week. We are the UK's leading professional body for Learning Technology, an independent voice for thousands of Members since 1993. Everyone is welcome #altc and membership starts from £55 pa https://t.co/MKXS4FzLKU https://t.co/FJ4ldmdkuq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edtech",
+					"indices": [
+						17,
+						24
+					]
+				},
+				{
+					"text": "altc",
+					"indices": [
+						201,
+						206
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "A_L_T",
+					"name": "ALT - alt.ac.uk",
+					"id": 41210876,
+					"id_str": "41210876",
+					"indices": [
+						38,
+						44
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MKXS4FzLKU",
+					"expanded_url": "https://www.alt.ac.uk/membership",
+					"display_url": "alt.ac.uk/membership",
+					"indices": [
+						241,
+						264
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1280107734562672600,
+					"id_str": "1280107734562672643",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/EcPbM8jWkAMHLCY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcPbM8jWkAMHLCY.jpg",
+					"url": "https://t.co/FJ4ldmdkuq",
+					"display_url": "pic.twitter.com/FJ4ldmdkuq",
+					"expanded_url": "https://twitter.com/MarenDeepwell/status/1280108493064867840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1280107734562672600,
+					"id_str": "1280107734562672643",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/EcPbM8jWkAMHLCY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcPbM8jWkAMHLCY.jpg",
+					"url": "https://t.co/FJ4ldmdkuq",
+					"display_url": "pic.twitter.com/FJ4ldmdkuq",
+					"expanded_url": "https://twitter.com/MarenDeepwell/status/1280108493064867840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 323173367,
+			"id_str": "323173367",
+			"name": "Dr Maren Deepwell",
+			"screen_name": "MarenDeepwell",
+			"location": "Cardiff, Wales",
+			"description": "CEO @A_L_T #altc, expert in open leadership of virtual teams & orgs, policy & Learning Technology CPD, Senior #CMALT, equality #femedtech she/her",
+			"url": "https://t.co/kMXdMTr9bt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kMXdMTr9bt",
+							"expanded_url": "http://marendeepwell.com",
+							"display_url": "marendeepwell.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4325,
+			"friends_count": 1878,
+			"listed_count": 197,
+			"created_at": "Fri Jun 24 11:30:35 +0000 2011",
+			"favourites_count": 22397,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11908,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1213925033690763273/JeGthJGa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1213925033690763273/JeGthJGa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/323173367/1594036676",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Glad to see more #edtech folk joining @A_L_T each week. We are the UK's leading professional body for Learning Technology, an independent voice for thousands of Members since 1993. Everyone is welcome #altc and membership starts from £55 pa https://t.co/MKXS4FzLKU https://t.co/FJ4ldmdkuq"
+	},
+	{
 		"created_at": "Fri Jul 03 14:52:46 +0000 2020",
 		"id": 1279065554163314700,
 		"id_str": "1279065554163314689",
