ae35747915ddc13c5b7ecfd8a6f343e71f603d6f
mhawksey <m.hawksey@gmail.com>
2020-07-14 02:10:54 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29823,
-	"created_at": "Sat Jul 11 01:10:53 +0000 2020",
+	"tweets": 29824,
+	"created_at": "Tue Jul 14 01:10:53 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_07.js
+++ tweets/data/js/tweets/2020_07.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 13 20:41:57 +0000 2020",
+		"id": 1282777078752931800,
+		"id_str": "1282777305442586625",
+		"full_text": "Google Apps Script – Stack Overflow growth trends and predictions  by @felipehoffa (contributed by @mhawksey) #GSuiteDevs #GoogleAppsScript https://t.co/Z0BTGHtawm https://t.co/UwHcRaeTgx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			163
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GSuiteDevs",
+					"indices": [
+						110,
+						121
+					]
+				},
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						122,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "felipehoffa",
+					"name": "Felipe Hoffa",
+					"id": 216735143,
+					"id_str": "216735143",
+					"indices": [
+						70,
+						82
+					]
+				},
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey #altc",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						99,
+						108
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Z0BTGHtawm",
+					"expanded_url": "https://pulse.appsscript.info/p/2020/07/google-apps-script-stack-overflow-growth-trends-and-predictions/",
+					"display_url": "pulse.appsscript.info/p/2020/07/goog…",
+					"indices": [
+						140,
+						163
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1282777077255569400,
+					"id_str": "1282777077255569409",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/media/Ec1W9I_VAAEbamO.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ec1W9I_VAAEbamO.png",
+					"url": "https://t.co/UwHcRaeTgx",
+					"display_url": "pic.twitter.com/UwHcRaeTgx",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1282777078752931840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 883,
+							"h": 537,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 414,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 883,
+							"h": 537,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1282777077255569400,
+					"id_str": "1282777077255569409",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/media/Ec1W9I_VAAEbamO.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ec1W9I_VAAEbamO.png",
+					"url": "https://t.co/UwHcRaeTgx",
+					"display_url": "pic.twitter.com/UwHcRaeTgx",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1282777078752931840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 883,
+							"h": 537,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 414,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 883,
+							"h": 537,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 322526439,
+			"id_str": "322526439",
+			"name": "AppsScriptInfo (supporting #GSuiteDevs)",
+			"screen_name": "AppsScriptInfo",
+			"location": "World",
+			"description": "A Google Apps Script Community account to support #GSuiteDevs. Curated by @mhawksey",
+			"url": "https://t.co/pEqPAHFH4F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEqPAHFH4F",
+							"expanded_url": "https://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 758,
+			"friends_count": 881,
+			"listed_count": 22,
+			"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+			"favourites_count": 504,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1799,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+			"profile_link_color": "4286F4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Google Apps Script – Stack Overflow growth trends and predictions  by @felipehoffa (contributed by @mhawksey) #GSuiteDevs #GoogleAppsScript https://t.co/Z0BTGHtawm https://t.co/UwHcRaeTgx"
+	},
+	{
 		"created_at": "Fri Jul 10 15:03:52 +0000 2020",
 		"id": 1281605062066876400,
 		"id_str": "1281605062066876418",
