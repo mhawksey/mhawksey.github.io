7392172b89ec2f381063ad331ec1f0c96f565635
mhawksey <m.hawksey@gmail.com>
2020-10-17 02:10:53 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30046,
-	"created_at": "Wed Oct 14 01:10:52 +0000 2020",
+	"tweets": 30047,
+	"created_at": "Sat Oct 17 01:10:52 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_10.js
+++ tweets/data/js/tweets/2020_10.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 16 08:29:10 +0000 2020",
+		"id": 1317019741840617500,
+		"id_str": "1317019741840617474",
+		"full_text": "Google Sheets’ new Smart Autofill will save you precious time entering data https://t.co/o1ru42VKoU #GoogleSheets https://t.co/sNGlgX1ldb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleSheets",
+					"indices": [
+						100,
+						113
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/o1ru42VKoU",
+					"expanded_url": "https://thenextweb.com/plugged/2020/10/16/google-sheets-new-smart-autofill-will-save-you-precious-time-entering-data/",
+					"display_url": "thenextweb.com/plugged/2020/1…",
+					"indices": [
+						76,
+						99
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1317019732231508000,
+					"id_str": "1317019732231507968",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Ekb-d_XW0AApF67.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Ekb-d_XW0AApF67.jpg",
+					"url": "https://t.co/sNGlgX1ldb",
+					"display_url": "pic.twitter.com/sNGlgX1ldb",
+					"expanded_url": "https://twitter.com/mhawksey/status/1317019741840617474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 638,
+							"h": 408,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 638,
+							"h": 408,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 638,
+							"h": 408,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1317019732231508000,
+					"id_str": "1317019732231507968",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Ekb-d_XW0AApF67.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Ekb-d_XW0AApF67.jpg",
+					"url": "https://t.co/sNGlgX1ldb",
+					"display_url": "pic.twitter.com/sNGlgX1ldb",
+					"expanded_url": "https://twitter.com/mhawksey/status/1317019741840617474/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 638,
+							"h": 408,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 638,
+							"h": 408,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 638,
+							"h": 408,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							319,
+							204
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Ekb-d_XW0AApF67.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Innovation, Comm & Tech @A_L_T | @GoogleDevExpert #GoogleAppsScript/#GSuiteDevs | Interests: Open Education, EdTech, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8959,
+			"friends_count": 2645,
+			"listed_count": 602,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 7103,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30195,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Google Sheets’ new Smart Autofill will save you precious time entering data https://t.co/o1ru42VKoU #GoogleSheets https://t.co/sNGlgX1ldb"
+	},
+	{
 		"created_at": "Tue Oct 13 20:18:02 +0000 2020",
 		"id": 1316110971073687600,
 		"id_str": "1316110971073687553",
