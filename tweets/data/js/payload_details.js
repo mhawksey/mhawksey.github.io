057b0e5352c22e95b63bd97ee53062bd22a7d669
mhawksey <m.hawksey@gmail.com>
2021-02-23 01:10:53 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30423,
-	"created_at": "Sat Feb 20 01:10:53 +0000 2021",
+	"tweets": 30424,
+	"created_at": "Tue Feb 23 01:10:53 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_02.js
+++ tweets/data/js/tweets/2021_02.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Mon Feb 22 22:32:44 +0000 2021",
+		"id": 1363980067588685800,
+		"id_str": "1363980067588685825",
+		"full_text": "@dapios is looking for an experienced Program Manager to support Google's Developers ecosystem who is also able to automate processes using #GoogleAppsScript. Based in Zurich (or Munich, London, Berlin)  @workspacedevs\n@GoogleDevExpert\n\nhttps://t.co/GBvvXPV4CE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						140,
+						157
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dapios",
+					"name": "Dawid Ostrowski",
+					"id": 95618010,
+					"id_str": "95618010",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "workspacedevs",
+					"name": "Google Workspace Developers",
+					"id": 111197966,
+					"id_str": "111197966",
+					"indices": [
+						204,
+						218
+					]
+				},
+				{
+					"screen_name": "GoogleDevExpert",
+					"name": "@GoogleDevExpert",
+					"id": 3003834689,
+					"id_str": "3003834689",
+					"indices": [
+						219,
+						235
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GBvvXPV4CE",
+					"expanded_url": "https://lnkd.in/eZ8Frqn",
+					"display_url": "lnkd.in/eZ8Frqn",
+					"indices": [
+						237,
+						260
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 95618010,
+		"in_reply_to_user_id_str": "95618010",
+		"in_reply_to_screen_name": "dapios",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey #altc",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Curator of @AppsScriptInfo |  @GoogleDevExpert in #GoogleAppsScript | Interests: EdTech Explorer, Open Education, Mashups, Analytics, Data, #altc, #FemEdTech",
+			"url": "https://t.co/OwPc0EbVqg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OwPc0EbVqg",
+							"expanded_url": "https://mashe.hawksey.info",
+							"display_url": "mashe.hawksey.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9115,
+			"friends_count": 2689,
+			"listed_count": 603,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 7397,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30646,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@dapios is looking for an experienced Program Manager to support Google's Developers ecosystem who is also able to automate processes using #GoogleAppsScript. Based in Zurich (or Munich, London, Berlin)  @workspacedevs\n@GoogleDevExpert\n\nhttps://t.co/GBvvXPV4CE"
+	},
+	{
 		"created_at": "Fri Feb 19 14:09:59 +0000 2021",
 		"id": 1362766381880590300,
 		"id_str": "1362766381880590346",
