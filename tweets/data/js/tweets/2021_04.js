ed2b38fbbce0f86c891cc830f5684a7f5f2044f6
mhawksey <m.hawksey@gmail.com>
2021-04-11 02:10:52 +0100

    Added 1 tweets

--- tweets/data/js/tweets/2021_04.js
+++ tweets/data/js/tweets/2021_04.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sat Apr 10 08:18:09 +0000 2021",
+		"id": 1380521051143221200,
+		"id_str": "1380797235718451202",
+		"full_text": "The full interactive online programme is now available for #OERxDomains21 registered participants to view at https://t.co/m3mv08vhSf #OER21 #Domains21 #OERxDomains https://t.co/9ecDvTsmf1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			163
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OERxDomains21",
+					"indices": [
+						59,
+						73
+					]
+				},
+				{
+					"text": "OER21",
+					"indices": [
+						133,
+						139
+					]
+				},
+				{
+					"text": "Domains21",
+					"indices": [
+						140,
+						150
+					]
+				},
+				{
+					"text": "OERxDomains",
+					"indices": [
+						151,
+						163
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/m3mv08vhSf",
+					"expanded_url": "https://oer21.oerconf.org/programme/",
+					"display_url": "oer21.oerconf.org/programme/",
+					"indices": [
+						109,
+						132
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1380520426879733800,
+					"id_str": "1380520426879733765",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1380520426879733765/pu/img/SQm3yZeIDdtdt77x.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1380520426879733765/pu/img/SQm3yZeIDdtdt77x.jpg",
+					"url": "https://t.co/9ecDvTsmf1",
+					"display_url": "pic.twitter.com/9ecDvTsmf1",
+					"expanded_url": "https://twitter.com/OERConf/status/1380521051143221250/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1380520426879733800,
+					"id_str": "1380520426879733765",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1380520426879733765/pu/img/SQm3yZeIDdtdt77x.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1380520426879733765/pu/img/SQm3yZeIDdtdt77x.jpg",
+					"url": "https://t.co/9ecDvTsmf1",
+					"display_url": "pic.twitter.com/9ecDvTsmf1",
+					"expanded_url": "https://twitter.com/OERConf/status/1380521051143221250/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 10000,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1380520426879733765/pu/pl/oZ-MiL52Sb4ne_oV.m3u8?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1380520426879733765/pu/vid/640x360/Z782me2OCfeDvfcn.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1380520426879733765/pu/vid/1280x720/1inNWOIBAe2sbOQc.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1380520426879733765/pu/vid/480x270/dlPuwcEY7F8PNyGn.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2695133612,
+			"id_str": "2695133612",
+			"name": "OER Open Education Conference",
+			"screen_name": "OERConf",
+			"location": "Online",
+			"description": "Official account for the OER Conference, organised by @A_L_T, UK, in 2021 together with the Domains Conference, online 21-22 April 2021.",
+			"url": "https://t.co/D9WFtVbcOQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/D9WFtVbcOQ",
+							"expanded_url": "https://oer21.oerconf.org/",
+							"display_url": "oer21.oerconf.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2724,
+			"friends_count": 1286,
+			"listed_count": 91,
+			"created_at": "Thu Jul 31 08:18:01 +0000 2014",
+			"favourites_count": 1692,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3464,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1361612711281442816/HykuxENc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1361612711281442816/HykuxENc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2695133612/1613468420",
+			"profile_link_color": "444444",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The full interactive online programme is now available for #OERxDomains21 registered participants to view at https://t.co/m3mv08vhSf #OER21 #Domains21 #OERxDomains https://t.co/9ecDvTsmf1"
+	},
+	{
 		"created_at": "Fri Apr 09 11:36:02 +0000 2021",
 		"id": 1380483921834619000,
 		"id_str": "1380484646098636802",
