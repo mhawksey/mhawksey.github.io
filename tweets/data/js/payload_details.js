a355e96f18c3ebc45159bdce67104de86c3f480a
mhawksey <m.hawksey@gmail.com>
2021-05-14 02:10:52 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30607,
-	"created_at": "Thu May 13 01:10:51 +0000 2021",
+	"tweets": 30608,
+	"created_at": "Fri May 14 01:10:51 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_05.js
+++ tweets/data/js/tweets/2021_05.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Thu May 13 18:43:36 +0000 2021",
+		"id": 1392913089008390100,
+		"id_str": "1392913433570463749",
+		"full_text": "Live in 20mins Totally Unscripted returns this time tomorrow to talk about #GoogleAppsScript in the Enterprise with @duizendstra and Niek Waarbroek from @NextNovate. https://t.co/QMQri8P6Ku Tune in live to ask questions  #WorkspaceDevs #AppsScript",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						75,
+						92
+					]
+				},
+				{
+					"text": "WorkspaceDevs",
+					"indices": [
+						221,
+						235
+					]
+				},
+				{
+					"text": "AppsScript",
+					"indices": [
+						236,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "duizendstra",
+					"name": "Jasper Duizendstra",
+					"id": 11434912,
+					"id_str": "11434912",
+					"indices": [
+						116,
+						128
+					]
+				},
+				{
+					"screen_name": "NextNovate",
+					"name": "NextNovate",
+					"id": 1151773981491585000,
+					"id_str": "1151773981491585026",
+					"indices": [
+						153,
+						164
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QMQri8P6Ku",
+					"expanded_url": "https://www.youtube.com/watch?v=N5zmeEsG2uw",
+					"display_url": "youtube.com/watch?v=N5zmeE…",
+					"indices": [
+						166,
+						189
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 322526439,
+			"id_str": "322526439",
+			"name": "AppsScriptInfo",
+			"screen_name": "AppsScriptInfo",
+			"location": "World",
+			"description": "A Google Apps Script Community account to support Google Workspace Devs. Curated by @mhawksey",
+			"url": "https://t.co/pEqPAHFH4F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEqPAHFH4F",
+							"expanded_url": "https://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1283,
+			"friends_count": 1261,
+			"listed_count": 39,
+			"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+			"favourites_count": 722,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2330,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134397720452575233/ZhyDgO7G_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+			"profile_link_color": "4286F4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Live in 20mins Totally Unscripted returns this time tomorrow to talk about #GoogleAppsScript in the Enterprise with @duizendstra and Niek Waarbroek from @NextNovate. https://t.co/QMQri8P6Ku Tune in live to ask questions  #WorkspaceDevs #AppsScript"
+	},
+	{
 		"created_at": "Wed May 12 19:02:50 +0000 2021",
 		"id": 1392554538863927300,
 		"id_str": "1392555886581800961",
