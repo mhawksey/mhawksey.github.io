1b9ef9b549fca1b5104f331cc8838b9b888f9bba
mhawksey <m.hawksey@gmail.com>
2021-05-29 02:10:58 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30634,
-	"created_at": "Fri May 28 01:10:52 +0000 2021",
+	"tweets": 30635,
+	"created_at": "Sat May 29 01:10:57 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_05.js
+++ tweets/data/js/tweets/2021_05.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Fri May 28 16:03:13 +0000 2021",
+		"id": 1398308890379423700,
+		"id_str": "1398308890379423748",
+		"full_text": "@JeremiahOsGo @theAAG @gsag_aag TAGS has a menu option to build an archive from a list of tweet id_str'ings . If you start a new TAGS Archive, add the user_location column then use the build from IDs menu option it creates an additional sheet where you can copy/paste the id_str from your existing archive :)",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			308
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JeremiahOsGo",
+					"name": "Jeremiah Osborne-Gowey",
+					"id": 43940052,
+					"id_str": "43940052",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "theAAG",
+					"name": "American Association of Geographers",
+					"id": 22382110,
+					"id_str": "22382110",
+					"indices": [
+						14,
+						21
+					]
+				},
+				{
+					"screen_name": "gsag_aag",
+					"name": "GSAG AAG",
+					"id": 1281672066664747000,
+					"id_str": "1281672066664747011",
+					"indices": [
+						22,
+						31
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1398305809768009700,
+		"in_reply_to_status_id_str": "1398305809768009734",
+		"in_reply_to_user_id": 43940052,
+		"in_reply_to_user_id_str": "43940052",
+		"in_reply_to_screen_name": "JeremiahOsGo",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Learning Design and Technology Lead @UoE_EFI | Curator of @AppsScriptInfo |  @GoogleDevExpert in #GoogleAppsScript | Interests: Hybrid/Fusion Learning",
+			"url": "https://t.co/ic511xTmhG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ic511xTmhG",
+							"expanded_url": "https://hawksey.info/blog/",
+							"display_url": "hawksey.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9185,
+			"friends_count": 2731,
+			"listed_count": 600,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 7611,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30908,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@JeremiahOsGo @theAAG @gsag_aag TAGS has a menu option to build an archive from a list of tweet id_str'ings . If you start a new TAGS Archive, add the user_location column then use the build from IDs menu option it creates an additional sheet where you can copy/paste the id_str from your existing archive :)"
+	},
+	{
 		"created_at": "Thu May 27 17:40:00 +0000 2021",
 		"id": 1397970858405159000,
 		"id_str": "1397970858405158922",
