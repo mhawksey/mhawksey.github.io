b94eb6b7a5dbb3b0a975b5ba539c7daf7b728a47
mhawksey <m.hawksey@gmail.com>
2021-09-05 01:20:32 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30746,
-	"created_at": "Fri Sep 03 00:20:31 +0000 2021",
+	"tweets": 30747,
+	"created_at": "Sun Sep 05 00:20:31 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_09.js
+++ tweets/data/js/tweets/2021_09.js
@@ -1,6 +1,315 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 04 12:05:32 +0000 2021",
+		"id": 1433762432460152800,
+		"id_str": "1434125475736064007",
+		"full_text": "Love @GoogleForEdu ? We are hiring for a new member of the team: https://t.co/JDLn73pHCm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GoogleForEdu",
+					"name": "Google for Education",
+					"id": 254218142,
+					"id_str": "254218142",
+					"indices": [
+						5,
+						18
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JDLn73pHCm",
+					"expanded_url": "https://twitter.com/CanopyCIC/status/1433758996960272400",
+					"display_url": "twitter.com/CanopyCIC/stat…",
+					"indices": [
+						65,
+						88
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 428411826,
+			"id_str": "428411826",
+			"name": "Andrew Caffrey",
+			"screen_name": "MrCaffrey",
+			"location": "United Kingdom",
+			"description": "CEO @CanopyCIC #Canopy - Google Certified Innovator Program Team #GoogleEI -  Volunteer Head of Compliance @Scouts",
+			"url": "https://t.co/52Z4NURlJl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/52Z4NURlJl",
+							"expanded_url": "http://mrcaffrey.com/",
+							"display_url": "mrcaffrey.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3936,
+			"friends_count": 1702,
+			"listed_count": 153,
+			"created_at": "Sun Dec 04 18:43:46 +0000 2011",
+			"favourites_count": 13761,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6870,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134003459827687424/wAWtaEAy_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134003459827687424/wAWtaEAy_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/428411826/1427720573",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1433758996960272400,
+		"quoted_status_id_str": "1433758996960272400",
+		"quoted_status_permalink": {
+			"url": "https://t.co/JDLn73pHCm",
+			"expanded": "https://twitter.com/CanopyCIC/status/1433758996960272400",
+			"display": "twitter.com/CanopyCIC/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Sep 03 11:49:17 +0000 2021",
+			"id": 1433758996960272400,
+			"id_str": "1433758996960272400",
+			"full_text": "Want to join the team at Canopy? \n\nWe are looking for a Product Lead for Workspace Skills https://t.co/ngN4QqQOrp https://t.co/K3dtFWI1zy",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				113
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ngN4QqQOrp",
+						"expanded_url": "https://uk.indeed.com/viewjob?t=product+lead+workspace+skills&jk=c7db5478b9baead1&_ga=2.142446193.2100581548.1630661859-1009274515.1630661859",
+						"display_url": "uk.indeed.com/viewjob?t=prod…",
+						"indices": [
+							90,
+							113
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1433758947119452200,
+						"id_str": "1433758947119452163",
+						"indices": [
+							114,
+							137
+						],
+						"media_url": "http://pbs.twimg.com/media/E-W8KoCX0AM6D7p.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E-W8KoCX0AM6D7p.jpg",
+						"url": "https://t.co/K3dtFWI1zy",
+						"display_url": "pic.twitter.com/K3dtFWI1zy",
+						"expanded_url": "https://twitter.com/CanopyCIC/status/1433758996960272400/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1433758947119452200,
+						"id_str": "1433758947119452163",
+						"indices": [
+							114,
+							137
+						],
+						"media_url": "http://pbs.twimg.com/media/E-W8KoCX0AM6D7p.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E-W8KoCX0AM6D7p.jpg",
+						"url": "https://t.co/K3dtFWI1zy",
+						"display_url": "pic.twitter.com/K3dtFWI1zy",
+						"expanded_url": "https://twitter.com/CanopyCIC/status/1433758996960272400/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1078608078071832600,
+				"id_str": "1078608078071832576",
+				"name": "Canopy",
+				"screen_name": "CanopyCIC",
+				"location": "United Kingdom",
+				"description": "We are Canopy - Passionate about using digital technology to enhance teaching and learning - Google for Education Professional Development Partner",
+				"url": "https://t.co/s6gxEI4vMK",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/s6gxEI4vMK",
+								"expanded_url": "https://www.canopy.education",
+								"display_url": "canopy.education",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1942,
+				"friends_count": 1810,
+				"listed_count": 26,
+				"created_at": "Fri Dec 28 11:06:20 +0000 2018",
+				"favourites_count": 2729,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2177,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1410515677560115205/7nZOdhar_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1410515677560115205/7nZOdhar_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1078608078071832576/1623166361",
+				"profile_link_color": "71AC43",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 24,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 7,
+		"favorite_count": 39,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Love @GoogleForEdu ? We are hiring for a new member of the team: https://t.co/JDLn73pHCm"
+	},
+	{
 		"created_at": "Thu Sep 02 15:58:37 +0000 2021",
 		"id": 1433459357333835800,
 		"id_str": "1433459357333835776",
