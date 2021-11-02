3a29fc1b9200ef0041d3411dbc00f6930da24163
mhawksey <m.hawksey@gmail.com>
2021-11-02 00:20:31 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30817,
-	"created_at": "Sun Oct 31 00:20:31 +0000 2021",
+	"tweets": 30818,
+	"created_at": "Tue Nov 02 00:20:31 +0000 2021",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2021_11.js
@@ -0,0 +1,151 @@
+Grailbird.data.tweets_2021_11 = 
+[
+	{
+		"created_at": "Mon Nov 01 22:03:34 +0000 2021",
+		"id": 1455294471567028200,
+		"id_str": "1455294471567028229",
+		"full_text": "@gavinhanel @IMTheNachoMan @schoraria911 @AppsScriptInfo I've pinged someone in the documentation team - when we had some of them on the show they also mentioned you can use the 'send feedback' button and tag as TotallyUnscripted to fast track comments https://t.co/LOjBK51tcG",
+		"truncated": false,
+		"display_text_range": [
+			57,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gavinhanel",
+					"name": "Gavin Hanel",
+					"id": 142400492,
+					"id_str": "142400492",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "IMTheNachoMan",
+					"name": "IMTheNachoMan",
+					"id": 14258013,
+					"id_str": "14258013",
+					"indices": [
+						12,
+						26
+					]
+				},
+				{
+					"screen_name": "schoraria911",
+					"name": "Sourabh Choraria",
+					"id": 1145333591867453400,
+					"id_str": "1145333591867453440",
+					"indices": [
+						27,
+						40
+					]
+				},
+				{
+					"screen_name": "AppsScriptInfo",
+					"name": "AppsScriptInfo",
+					"id": 322526439,
+					"id_str": "322526439",
+					"indices": [
+						41,
+						56
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/LOjBK51tcG",
+					"expanded_url": "https://youtu.be/lHGypmT29RY?t=1929",
+					"display_url": "youtu.be/lHGypmT29RY?t=…",
+					"indices": [
+						253,
+						276
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1455158177553584000,
+		"in_reply_to_status_id_str": "1455158177553584136",
+		"in_reply_to_user_id": 142400492,
+		"in_reply_to_user_id_str": "142400492",
+		"in_reply_to_screen_name": "gavinhanel",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Learning Design and Technology Lead @UoE_EFI | Curator of @AppsScriptInfo |  @GoogleDevExpert in #GoogleAppsScript | Interests: Hybrid/Fusion Learning",
+			"url": "https://t.co/ic511xTmhG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ic511xTmhG",
+							"expanded_url": "https://hawksey.info/blog/",
+							"display_url": "hawksey.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9283,
+			"friends_count": 2768,
+			"listed_count": 596,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 7827,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 31143,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@gavinhanel @IMTheNachoMan @schoraria911 @AppsScriptInfo I've pinged someone in the documentation team - when we had some of them on the show they also mentioned you can use the 'send feedback' button and tag as TotallyUnscripted to fast track comments https://t.co/LOjBK51tcG"
+	}
+]
