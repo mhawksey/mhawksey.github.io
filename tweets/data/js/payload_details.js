b6c9980747b0667ec37e41fc57105bda30013831
mhawksey <m.hawksey@gmail.com>
2021-11-17 00:20:51 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30829,
-	"created_at": "Thu Nov 11 00:20:30 +0000 2021",
+	"tweets": 30830,
+	"created_at": "Wed Nov 17 00:20:50 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_11.js
+++ tweets/data/js/tweets/2021_11.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 16 08:21:42 +0000 2021",
+		"id": 1460486633967419400,
+		"id_str": "1460523457574285313",
+		"full_text": "Happy to release my new Git and Github tutorial. This tutorial will cover all the concepts of Git and Github and get you started on Git and Github.\n\n#git #github #githubtutorial @github @GitHubIndia \nhttps://t.co/8MHjTn4zE2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			223
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "git",
+					"indices": [
+						149,
+						153
+					]
+				},
+				{
+					"text": "github",
+					"indices": [
+						154,
+						161
+					]
+				},
+				{
+					"text": "githubtutorial",
+					"indices": [
+						162,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "github",
+					"name": "GitHub",
+					"id": 13334762,
+					"id_str": "13334762",
+					"indices": [
+						178,
+						185
+					]
+				},
+				{
+					"screen_name": "GitHubIndia",
+					"name": "GitHubIndia",
+					"id": 1216780531423010800,
+					"id_str": "1216780531423010816",
+					"indices": [
+						186,
+						198
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8MHjTn4zE2",
+					"expanded_url": "https://aryanirani123.medium.com/git-and-github-tutorial-aryan-irani-61b9c34ec855",
+					"display_url": "aryanirani123.medium.com/git-and-github…",
+					"indices": [
+						200,
+						223
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815158584375840800,
+			"id_str": "815158584375840768",
+			"name": "Aryan Irani",
+			"screen_name": "aryanirani123",
+			"location": "",
+			"description": "Freelance writer @TalarianHQ\nFor collaborations: aryanirani123@gmail.com\n#GoogleAppsScript #GoogleWorkspace\nI write and create on the Internet",
+			"url": "https://t.co/zCGOiUd3Yq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zCGOiUd3Yq",
+							"expanded_url": "https://bit.ly/3guDaxR",
+							"display_url": "bit.ly/3guDaxR",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 141,
+			"friends_count": 170,
+			"listed_count": 1,
+			"created_at": "Sat Dec 31 11:32:06 +0000 2016",
+			"favourites_count": 1021,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 245,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1460564776413642753/G8rGo0br_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1460564776413642753/G8rGo0br_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815158584375840768/1623384886",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Happy to release my new Git and Github tutorial. This tutorial will cover all the concepts of Git and Github and get you started on Git and Github.\n\n#git #github #githubtutorial @github @GitHubIndia \nhttps://t.co/8MHjTn4zE2"
+	},
+	{
 		"created_at": "Wed Nov 10 14:29:57 +0000 2021",
 		"id": 1458431649759248400,
 		"id_str": "1458441804232151041",
