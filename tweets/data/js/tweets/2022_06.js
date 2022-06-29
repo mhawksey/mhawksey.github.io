26284bc73cc862cebab79285414ddaa2f1cb07b8
mhawksey <m.hawksey@gmail.com>
2022-06-29 01:20:32 +0100

    Added 1 tweets

--- tweets/data/js/tweets/2022_06.js
+++ tweets/data/js/tweets/2022_06.js
@@ -1,6 +1,183 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Tue Jun 28 14:17:58 +0000 2022",
+		"id": 1541787997867319300,
+		"id_str": "1541787997867319297",
+		"full_text": "@m1ckc3b @barrielroberts @ChanelGreco @philly_pb @LifeOfSpy @gorankukurin @labnol Personally, I would say not essential but experience of computational thinking/practice really helps (I started having done zero JavaScript, but had worked in Java and PHP).",
+		"truncated": false,
+		"display_text_range": [
+			82,
+			255
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "m1ckc3b",
+					"name": "mickceb",
+					"id": 869571039726104600,
+					"id_str": "869571039726104576",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "barrielroberts",
+					"name": "Baz Roberts",
+					"id": 4409428666,
+					"id_str": "4409428666",
+					"indices": [
+						9,
+						24
+					]
+				},
+				{
+					"screen_name": "ChanelGreco",
+					"name": "Chanel Greco",
+					"id": 1710909806,
+					"id_str": "1710909806",
+					"indices": [
+						25,
+						37
+					]
+				},
+				{
+					"screen_name": "philly_pb",
+					"name": "Phillip Bainbridge",
+					"id": 385609648,
+					"id_str": "385609648",
+					"indices": [
+						38,
+						48
+					]
+				},
+				{
+					"screen_name": "LifeOfSpy",
+					"name": "Yagisanatode",
+					"id": 2346078620,
+					"id_str": "2346078620",
+					"indices": [
+						49,
+						59
+					]
+				},
+				{
+					"screen_name": "gorankukurin",
+					"name": "Goran Kukurin",
+					"id": 3104432578,
+					"id_str": "3104432578",
+					"indices": [
+						60,
+						73
+					]
+				},
+				{
+					"screen_name": "labnol",
+					"name": "Amit Agarwal",
+					"id": 724473,
+					"id_str": "724473",
+					"indices": [
+						74,
+						81
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1541772197647339500,
+		"in_reply_to_status_id_str": "1541772197647339525",
+		"in_reply_to_user_id": 869571039726104600,
+		"in_reply_to_user_id_str": "869571039726104576",
+		"in_reply_to_screen_name": "m1ckc3b",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Curator/creator @AppsScriptInfo + https://t.co/wvb4AKjEQc | Google Cloud Champion Innovator and @GoogleDevExpert in #GoogleAppsScript/#Workspace",
+			"url": "https://t.co/ic511xTmhG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ic511xTmhG",
+							"expanded_url": "https://hawksey.info/blog/",
+							"display_url": "hawksey.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/wvb4AKjEQc",
+							"expanded_url": "http://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								34,
+								57
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9440,
+			"friends_count": 2812,
+			"listed_count": 587,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 8004,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 31420,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@m1ckc3b @barrielroberts @ChanelGreco @philly_pb @LifeOfSpy @gorankukurin @labnol Personally, I would say not essential but experience of computational thinking/practice really helps (I started having done zero JavaScript, but had worked in Java and PHP)."
+	},
+	{
 		"created_at": "Thu Jun 23 17:38:46 +0000 2022",
 		"id": 1540026591857840000,
 		"id_str": "1540026591857840129",
