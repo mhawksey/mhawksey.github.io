0ccebb8af9e07ccfde9e3ceb0750a656fd34d371
mhawksey <m.hawksey@gmail.com>
2022-07-04 01:20:32 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31031,
-	"created_at": "Wed Jun 29 00:20:31 +0000 2022",
+	"tweets": 31032,
+	"created_at": "Mon Jul 04 00:20:31 +0000 2022",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2022_07.js
@@ -0,0 +1,157 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Sun Jul 03 05:30:24 +0000 2022",
+		"id": 1543286630840254500,
+		"id_str": "1543467171220799488",
+		"full_text": "The new #Screencast tool is now rolling out to #Chromebooks on #ChromeOS 103!\nIt enables you to \n📹Record your screen\n📄Auto generate a transcript\n🧭Navigate video using the transcript \n🧑‍🏫Share to #Classroom or #Drive\n\nCheck it out https://t.co/3lJ9lAPjMF 🎬\n@GoogleForEdu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Screencast",
+					"indices": [
+						8,
+						19
+					]
+				},
+				{
+					"text": "Chromebooks",
+					"indices": [
+						47,
+						59
+					]
+				},
+				{
+					"text": "ChromeOS",
+					"indices": [
+						63,
+						72
+					]
+				},
+				{
+					"text": "Classroom",
+					"indices": [
+						195,
+						205
+					]
+				},
+				{
+					"text": "Drive",
+					"indices": [
+						209,
+						215
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GoogleForEdu",
+					"name": "Google for Education",
+					"id": 254218142,
+					"id_str": "254218142",
+					"indices": [
+						256,
+						269
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3lJ9lAPjMF",
+					"expanded_url": "https://youtu.be/FsoAAHXTCjo",
+					"display_url": "youtu.be/FsoAAHXTCjo",
+					"indices": [
+						230,
+						253
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1085834728228442100,
+			"id_str": "1085834728228442112",
+			"name": "Peter Horner #EdTech50",
+			"screen_name": "peterghorner",
+			"location": "",
+			"description": "IT Manager for @bartonpeveril #FE | Google Certified Innovator & Trainer #GoogleEI #VIA20 #GoogleET  | #Edtech50 | #AppsScript | MSc IT | Dad of two girls",
+			"url": "https://t.co/S6O4V0UhIi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/S6O4V0UhIi",
+							"expanded_url": "https://www.youtube.com/channel/UC7XYCCS9FPlqdCoXxu0C6Mg",
+							"display_url": "youtube.com/channel/UC7XYC…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3305,
+			"friends_count": 2194,
+			"listed_count": 35,
+			"created_at": "Thu Jan 17 09:42:27 +0000 2019",
+			"favourites_count": 24861,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5649,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1507110713331961869/D1DDbRfH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1507110713331961869/D1DDbRfH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1085834728228442112/1648160697",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 27,
+		"favorite_count": 83,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The new #Screencast tool is now rolling out to #Chromebooks on #ChromeOS 103!\nIt enables you to \n📹Record your screen\n📄Auto generate a transcript\n🧭Navigate video using the transcript \n🧑‍🏫Share to #Classroom or #Drive\n\nCheck it out https://t.co/3lJ9lAPjMF 🎬\n@GoogleForEdu"
+	}
+]
