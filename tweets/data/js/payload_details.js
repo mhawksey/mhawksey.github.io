660872f6d579a7773701fe36c169a3b0e6a7c4c8
mhawksey <m.hawksey@gmail.com>
2022-12-15 00:20:33 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31126,
-	"created_at": "Wed Dec 14 00:20:31 +0000 2022",
+	"tweets": 31127,
+	"created_at": "Thu Dec 15 00:20:32 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_12.js
+++ tweets/data/js/tweets/2022_12.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Wed Dec 14 08:00:45 +0000 2022",
+		"id": 1602936617748123600,
+		"id_str": "1602936617748123648",
+		"full_text": "@PalaciosAlexito @KiranPr00150087 In transit it is as secure as however your Gmail account has been setup because the script effectively sends emails as if it is the user running the code. Clearly consideration is required in how you manage the Google Sheet, limiting access etc.",
+		"truncated": false,
+		"display_text_range": [
+			34,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PalaciosAlexito",
+					"name": "Alex Palacios Cuadrado",
+					"id": 231550981,
+					"id_str": "231550981",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "KiranPr00150087",
+					"name": "Kiran Prasad",
+					"id": 1539142766613127200,
+					"id_str": "1539142766613127172",
+					"indices": [
+						17,
+						33
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1602905137105830000,
+		"in_reply_to_status_id_str": "1602905137105829889",
+		"in_reply_to_user_id": 231550981,
+		"in_reply_to_user_id_str": "231550981",
+		"in_reply_to_screen_name": "PalaciosAlexito",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Curator/creator @AppsScriptInfo + https://t.co/wvb4AKjEQc | Google Cloud Champion Innovator and @GoogleDevExpert in #GoogleAppsScript/#Workspace",
+			"url": "https://t.co/ic511xTmhG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ic511xTmhG",
+							"expanded_url": "https://hawksey.info/blog/",
+							"display_url": "hawksey.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/wvb4AKjEQc",
+							"expanded_url": "http://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								34,
+								57
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9476,
+			"friends_count": 2794,
+			"listed_count": 590,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 8011,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 31528,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@PalaciosAlexito @KiranPr00150087 In transit it is as secure as however your Gmail account has been setup because the script effectively sends emails as if it is the user running the code. Clearly consideration is required in how you manage the Google Sheet, limiting access etc."
+	},
+	{
 		"created_at": "Tue Dec 13 07:16:22 +0000 2022",
 		"id": 1602428867217862700,
 		"id_str": "1602563062514450434",
