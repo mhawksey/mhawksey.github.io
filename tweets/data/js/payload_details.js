943d1a10cc6abbba450488546b8ed8c8aae82fca
mhawksey <m.hawksey@gmail.com>
2023-01-23 00:20:36 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31134,
-	"created_at": "Sat Jan 21 00:20:31 +0000 2023",
+	"tweets": 31135,
+	"created_at": "Mon Jan 23 00:20:35 +0000 2023",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2023_01.js
+++ tweets/data/js/tweets/2023_01.js
@@ -1,6 +1,225 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Sun Jan 22 15:56:18 +0000 2023",
+		"id": 1617189422008606700,
+		"id_str": "1617189422008606724",
+		"full_text": "Made a helper class for the #AppSheet API in #GoogleAppsScript and now have  #GoogleSheet super powers. Find, add and edit data in a couple of lines of code. (Plus if I need to change out my data source I need to change zero lines of code!)\nhttps://t.co/HNplQTXq5T https://t.co/JhHVQlclTL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AppSheet",
+					"indices": [
+						28,
+						37
+					]
+				},
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						45,
+						62
+					]
+				},
+				{
+					"text": "GoogleSheet",
+					"indices": [
+						77,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HNplQTXq5T",
+					"expanded_url": "https://support.google.com/appsheet/answer/10105398",
+					"display_url": "support.google.com/appsheet/answe…",
+					"indices": [
+						241,
+						264
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1617188979538858000,
+					"id_str": "1617188979538857988",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FnFozwQXEAQhfsi.png",
+					"media_url_https": "https://pbs.twimg.com/media/FnFozwQXEAQhfsi.png",
+					"url": "https://t.co/JhHVQlclTL",
+					"display_url": "pic.twitter.com/JhHVQlclTL",
+					"expanded_url": "https://twitter.com/mhawksey/status/1617189422008606724/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 653,
+							"h": 403,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 653,
+							"h": 403,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 653,
+							"h": 403,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1617188979538858000,
+					"id_str": "1617188979538857988",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FnFozwQXEAQhfsi.png",
+					"media_url_https": "https://pbs.twimg.com/media/FnFozwQXEAQhfsi.png",
+					"url": "https://t.co/JhHVQlclTL",
+					"display_url": "pic.twitter.com/JhHVQlclTL",
+					"expanded_url": "https://twitter.com/mhawksey/status/1617189422008606724/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 653,
+							"h": 403,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 653,
+							"h": 403,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 653,
+							"h": 403,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Curator/creator @AppsScriptInfo + https://t.co/wvb4AKjEQc | Google Cloud Champion Innovator and @GoogleDevExpert in #GoogleAppsScript/#Workspace",
+			"url": "https://t.co/ic511xTmhG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ic511xTmhG",
+							"expanded_url": "https://hawksey.info/blog/",
+							"display_url": "hawksey.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/wvb4AKjEQc",
+							"expanded_url": "http://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								34,
+								57
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9480,
+			"friends_count": 2790,
+			"listed_count": 587,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 7997,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 31532,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			322526439
+		],
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Made a helper class for the #AppSheet API in #GoogleAppsScript and now have  #GoogleSheet super powers. Find, add and edit data in a couple of lines of code. (Plus if I need to change out my data source I need to change zero lines of code!)\nhttps://t.co/HNplQTXq5T https://t.co/JhHVQlclTL"
+	},
+	{
 		"created_at": "Fri Jan 20 18:42:00 +0000 2023",
 		"id": 1616506345087242200,
 		"id_str": "1616506345087242251",
