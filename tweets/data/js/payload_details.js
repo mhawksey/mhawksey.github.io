66ce416390c20a15a1b239c670540f7ec2cf8ae8
mhawksey <m.hawksey@gmail.com>
2023-02-02 00:20:32 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31136,
-	"created_at": "Wed Feb 01 00:20:31 +0000 2023",
+	"tweets": 31137,
+	"created_at": "Thu Feb 02 00:20:31 +0000 2023",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2023_02.js
@@ -0,0 +1,241 @@
+Grailbird.data.tweets_2023_02 = 
+[
+	{
+		"created_at": "Wed Feb 01 09:16:06 +0000 2023",
+		"id": 1620548491679527000,
+		"id_str": "1620712586932281344",
+		"full_text": "SpeedStore: Blazingly fast Properties storage for Google Apps Script ⚡ by @joshsny (contributed by @mhawksey) #GoogleWorkspace @WorkspaceDevs #GoogleAppsScript #AppsScript https://t.co/DkkRZeKmuY https://t.co/nqJQmeCAbM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleWorkspace",
+					"indices": [
+						110,
+						126
+					]
+				},
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						142,
+						159
+					]
+				},
+				{
+					"text": "AppsScript",
+					"indices": [
+						160,
+						171
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "joshsny",
+					"name": "Joshua Snyder",
+					"id": 1484535384533049300,
+					"id_str": "1484535384533049347",
+					"indices": [
+						74,
+						82
+					]
+				},
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						99,
+						108
+					]
+				},
+				{
+					"screen_name": "workspacedevs",
+					"name": "Google Workspace Developers",
+					"id": 111197966,
+					"id_str": "111197966",
+					"indices": [
+						127,
+						141
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DkkRZeKmuY",
+					"expanded_url": "https://pulse.appsscript.info/p/2023/01/speedstore-blazingly-fast-properties-storage-for-google-apps-script-%e2%9a%a1/",
+					"display_url": "pulse.appsscript.info/p/2023/01/spee…",
+					"indices": [
+						172,
+						195
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1620548488693190700,
+					"id_str": "1620548488693190656",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn1YQ7HakAATwiG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn1YQ7HakAATwiG.jpg",
+					"url": "https://t.co/nqJQmeCAbM",
+					"display_url": "pic.twitter.com/nqJQmeCAbM",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1620548491679526912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1620548488693190700,
+					"id_str": "1620548488693190656",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn1YQ7HakAATwiG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn1YQ7HakAATwiG.jpg",
+					"url": "https://t.co/nqJQmeCAbM",
+					"display_url": "pic.twitter.com/nqJQmeCAbM",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1620548491679526912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 322526439,
+			"id_str": "322526439",
+			"name": "AppsScriptInfo",
+			"screen_name": "AppsScriptInfo",
+			"location": "World",
+			"description": "A Google Apps Script Community account to support Google Workspace Devs. Curated by @mhawksey",
+			"url": "https://t.co/db8P5bhqPd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/db8P5bhqPd",
+							"expanded_url": "https://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2238,
+			"friends_count": 1536,
+			"listed_count": 55,
+			"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+			"favourites_count": 986,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3174,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1509531998359982080/BbebJ-ka_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1509531998359982080/BbebJ-ka_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+			"profile_link_color": "4286F4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SpeedStore: Blazingly fast Properties storage for Google Apps Script ⚡ by @joshsny (contributed by @mhawksey) #GoogleWorkspace @WorkspaceDevs #GoogleAppsScript #AppsScript https://t.co/DkkRZeKmuY https://t.co/nqJQmeCAbM"
+	}
+]
