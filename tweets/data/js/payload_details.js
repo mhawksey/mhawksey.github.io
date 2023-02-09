8ae4dcf81c36b0e982a988a14aa49e39af9ebe60
mhawksey <m.hawksey@gmail.com>
2023-02-09 00:20:33 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31143,
-	"created_at": "Wed Feb 08 00:20:31 +0000 2023",
+	"tweets": 31144,
+	"created_at": "Thu Feb 09 00:20:32 +0000 2023",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2023_02.js
+++ tweets/data/js/tweets/2023_02.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Wed Feb 08 12:27:54 +0000 2023",
+		"id": 1623297570574327800,
+		"id_str": "1623297570574327814",
+		"full_text": "@daniel_datsenko Not familiar with 3P tools but you might be able to modify to incorporate https://t.co/lEqBebmCxy and  https://t.co/gj0URNlNRX",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			143
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "daniel_datsenko",
+					"name": "datsenko.eth",
+					"id": 879072765227171800,
+					"id_str": "879072765227171841",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lEqBebmCxy",
+					"expanded_url": "https://dev.to/ravgeetdhillon/how-to-track-email-opens-with-google-apps-script-2lah",
+					"display_url": "dev.to/ravgeetdhillon…",
+					"indices": [
+						91,
+						114
+					]
+				},
+				{
+					"url": "https://t.co/gj0URNlNRX",
+					"expanded_url": "https://www.benlcollins.com/apps-script/track-link-clicks/",
+					"display_url": "benlcollins.com/apps-script/tr…",
+					"indices": [
+						120,
+						143
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1623293553517510700,
+		"in_reply_to_status_id_str": "1623293553517510659",
+		"in_reply_to_user_id": 879072765227171800,
+		"in_reply_to_user_id_str": "879072765227171841",
+		"in_reply_to_screen_name": "daniel_datsenko",
+		"user": {
+			"id": 13046992,
+			"id_str": "13046992",
+			"name": "Martin Hawksey",
+			"screen_name": "mhawksey",
+			"location": "Edinburgh, Scotland",
+			"description": "Curator/creator @AppsScriptInfo + https://t.co/wvb4AKjEQc | Google Cloud Champion Innovator and @GoogleDevExpert in #GoogleAppsScript/#Workspace",
+			"url": "https://t.co/ic511xTmhG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ic511xTmhG",
+							"expanded_url": "https://hawksey.info/blog/",
+							"display_url": "hawksey.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/wvb4AKjEQc",
+							"expanded_url": "http://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								34,
+								57
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9490,
+			"friends_count": 2790,
+			"listed_count": 585,
+			"created_at": "Mon Feb 04 13:30:17 +0000 2008",
+			"favourites_count": 7987,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 31543,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2390851993/xu6aptqy6a8rb2h2w5by_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/13046992/1478558874",
+			"profile_link_color": "4099FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@daniel_datsenko Not familiar with 3P tools but you might be able to modify to incorporate https://t.co/lEqBebmCxy and  https://t.co/gj0URNlNRX"
+	},
+	{
 		"created_at": "Tue Feb 07 07:47:57 +0000 2023",
 		"id": 1622864728094646300,
 		"id_str": "1622864728094646274",
