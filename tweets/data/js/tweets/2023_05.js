644c9ea094fcaebf7030317e71ad6971a9c1e50f
mhawksey <m.hawksey@gmail.com>
2023-05-13 01:20:32 +0100

    Added 1 tweets

--- tweets/data/js/tweets/2023_05.js
+++ tweets/data/js/tweets/2023_05.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Fri May 12 19:00:38 +0000 2023",
+		"id": 1657091461035294700,
+		"id_str": "1657098476830744598",
+		"full_text": "Meet our amazing speaker Martin Hawksey @mhawksey ! 🙌 \n\nMartin will talk about how AppSheet can be integrated with other Google Workspace APIs, showcasing how AppSheet can be used to interface a Grab n Go Chrome Loaner solution.\n\n🎟️RSVP: https://t.co/to49HNAjOD\n \n#GoogleIO #gdg https://t.co/Vp69FbOrxE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleIO",
+					"indices": [
+						264,
+						273
+					]
+				},
+				{
+					"text": "gdg",
+					"indices": [
+						274,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						40,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/to49HNAjOD",
+					"expanded_url": "https://meetup.com/gdg-edinburgh/events/293121901/",
+					"display_url": "meetup.com/gdg-edinburgh/…",
+					"indices": [
+						238,
+						261
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1657091323571101700,
+					"id_str": "1657091323571101711",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Fv8rx3XWYA8X-XM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fv8rx3XWYA8X-XM.jpg",
+					"url": "https://t.co/Vp69FbOrxE",
+					"display_url": "pic.twitter.com/Vp69FbOrxE",
+					"expanded_url": "https://twitter.com/edinburgh_GDG/status/1657091461035294720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 595,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 337,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1404,
+							"h": 696,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1657091323571101700,
+					"id_str": "1657091323571101711",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Fv8rx3XWYA8X-XM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fv8rx3XWYA8X-XM.jpg",
+					"url": "https://t.co/Vp69FbOrxE",
+					"display_url": "pic.twitter.com/Vp69FbOrxE",
+					"expanded_url": "https://twitter.com/edinburgh_GDG/status/1657091461035294720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 595,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 337,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1404,
+							"h": 696,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1195363981898203100,
+			"id_str": "1195363981898203136",
+			"name": "GDG Edinburgh",
+			"screen_name": "edinburgh_GDG",
+			"location": "Edinburgh, United Kingdom",
+			"description": "Google Developer Group - Edinburgh | we come together to learn, breather and get inspired| stay tuned for our event updates 🏴󠁧󠁢󠁳󠁣󠁴󠁿",
+			"url": "https://t.co/eGBkFur2so",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/eGBkFur2so",
+							"expanded_url": "https://gdgedinburgh.start.page/",
+							"display_url": "gdgedinburgh.start.page",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 52,
+			"friends_count": 128,
+			"listed_count": 1,
+			"created_at": "Fri Nov 15 15:33:05 +0000 2019",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 34,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1195365313354842112/dDdp5_ka_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1195365313354842112/dDdp5_ka_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1195363981898203136/1683297223",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Meet our amazing speaker Martin Hawksey @mhawksey ! 🙌 \n\nMartin will talk about how AppSheet can be integrated with other Google Workspace APIs, showcasing how AppSheet can be used to interface a Grab n Go Chrome Loaner solution.\n\n🎟️RSVP: https://t.co/to49HNAjOD\n \n#GoogleIO #gdg https://t.co/Vp69FbOrxE"
+	},
+	{
 		"created_at": "Tue May 09 18:33:03 +0000 2023",
 		"id": 1656004371774152700,
 		"id_str": "1656004371774152721",
