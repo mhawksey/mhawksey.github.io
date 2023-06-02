821048060c1b2ac5ace50df9399f12eb1965bc08
mhawksey <m.hawksey@gmail.com>
2023-06-02 01:20:33 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31171,
-	"created_at": "Thu Jun 01 00:20:31 +0000 2023",
+	"tweets": 31172,
+	"created_at": "Fri Jun 02 00:20:32 +0000 2023",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2023_06.js
@@ -0,0 +1,245 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Thu Jun 01 18:05:26 +0000 2023",
+		"id": 1664331306715455500,
+		"id_str": "1664332340775075840",
+		"full_text": "The #GoogleChat API is now generally available! 🚀(via @mhawksey 🙏).\n\n📌 https://t.co/l744amOKaF\n\nNow everyone can perform user authentication to manage spaces, users, messages... #AppsScript time! 🥳 https://t.co/3bZOWAhKeg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			197
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleChat",
+					"indices": [
+						4,
+						15
+					]
+				},
+				{
+					"text": "AppsScript",
+					"indices": [
+						178,
+						189
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						54,
+						63
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/l744amOKaF",
+					"expanded_url": "https://developers.google.com/chat/docs/release-notes#May_25_2023",
+					"display_url": "developers.google.com/chat/docs/rele…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1664324082664456200,
+					"id_str": "1664324082664456215",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1664324082664456215/pu/img/BOWJQ3aySsbRs5f7.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1664324082664456215/pu/img/BOWJQ3aySsbRs5f7.jpg",
+					"url": "https://t.co/3bZOWAhKeg",
+					"display_url": "pic.twitter.com/3bZOWAhKeg",
+					"expanded_url": "https://twitter.com/pfelipm/status/1664331306715455490/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 850,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1664324082664456200,
+					"id_str": "1664324082664456215",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1664324082664456215/pu/img/BOWJQ3aySsbRs5f7.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1664324082664456215/pu/img/BOWJQ3aySsbRs5f7.jpg",
+					"url": "https://t.co/3bZOWAhKeg",
+					"display_url": "pic.twitter.com/3bZOWAhKeg",
+					"expanded_url": "https://twitter.com/pfelipm/status/1664331306715455490/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 850,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 600,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							17,
+							12
+						],
+						"duration_millis": 10150,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1664324082664456215/pu/vid/850x600/fg5zx5mOaBonL9HS.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1664324082664456215/pu/pl/xMDP4vJomwLd3vln.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1664324082664456215/pu/vid/382x270/hQ596guQT_T1Nbx5.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1664324082664456215/pu/vid/510x360/r1aO3oyu1QlxH21N.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 127973484,
+			"id_str": "127973484",
+			"name": "Pablo Felip",
+			"screen_name": "pfelipm",
+			"location": "Castelló, España",
+			"description": "Escéptico practicante en constante búsqueda de #subalegrías. Coordinador en @GEGSpain, @GoogleDevExpert #GoogleWorkspace, @NotionHQ Ambassador.",
+			"url": "https://t.co/WbDgTbhvei",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WbDgTbhvei",
+							"expanded_url": "https://pablofelip.online",
+							"display_url": "pablofelip.online",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3541,
+			"friends_count": 384,
+			"listed_count": 109,
+			"created_at": "Tue Mar 30 19:15:16 +0000 2010",
+			"favourites_count": 10419,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9636,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DBE9ED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551100700142309377/zU7BRIq3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551100700142309377/zU7BRIq3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/127973484/1443353173",
+			"profile_link_color": "CC3366",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E6F6F9",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The #GoogleChat API is now generally available! 🚀(via @mhawksey 🙏).\n\n📌 https://t.co/l744amOKaF\n\nNow everyone can perform user authentication to manage spaces, users, messages... #AppsScript time! 🥳 https://t.co/3bZOWAhKeg"
+	}
+]
