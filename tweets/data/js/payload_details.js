bc6e5ddd6b9a6831118e7402a1beda8901b9f567
mhawksey <m.hawksey@gmail.com>
2023-06-06 01:20:32 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31172,
-	"created_at": "Fri Jun 02 00:20:32 +0000 2023",
+	"tweets": 31173,
+	"created_at": "Tue Jun 06 00:20:31 +0000 2023",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2023_06.js
+++ tweets/data/js/tweets/2023_06.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Mon Jun 05 21:19:20 +0000 2023",
+		"id": 1665828737768915000,
+		"id_str": "1665830687969095683",
+		"full_text": "Copy macros to other Google Sheets (and how to centrally create/update Apps Script code to Google Workspace editors) (contributed by @mhawksey) #GoogleWorkspace @WorkspaceDevs #GoogleAppsScript #AppsScript https://t.co/0gGHYx8h5Q https://t.co/lEfpRDegbj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleWorkspace",
+					"indices": [
+						144,
+						160
+					]
+				},
+				{
+					"text": "GoogleAppsScript",
+					"indices": [
+						176,
+						193
+					]
+				},
+				{
+					"text": "AppsScript",
+					"indices": [
+						194,
+						205
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						133,
+						142
+					]
+				},
+				{
+					"screen_name": "workspacedevs",
+					"name": "Google Workspace Developers",
+					"id": 111197966,
+					"id_str": "111197966",
+					"indices": [
+						161,
+						175
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/0gGHYx8h5Q",
+					"expanded_url": "https://pulse.appsscript.info/p/2023/06/copy-macros-to-other-google-sheets-and-how-to-centrally-create-update-apps-script-code-to-google-workspace-editors/",
+					"display_url": "pulse.appsscript.info/p/2023/06/copy…",
+					"indices": [
+						206,
+						229
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1665828735545925600,
+					"id_str": "1665828735545925632",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/Fx42aLbaYAA6NCE.png",
+					"media_url_https": "https://pbs.twimg.com/media/Fx42aLbaYAA6NCE.png",
+					"url": "https://t.co/lEfpRDegbj",
+					"display_url": "pic.twitter.com/lEfpRDegbj",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1665828737768914944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 588,
+							"h": 488,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 588,
+							"h": 488,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 588,
+							"h": 488,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1665828735545925600,
+					"id_str": "1665828735545925632",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/Fx42aLbaYAA6NCE.png",
+					"media_url_https": "https://pbs.twimg.com/media/Fx42aLbaYAA6NCE.png",
+					"url": "https://t.co/lEfpRDegbj",
+					"display_url": "pic.twitter.com/lEfpRDegbj",
+					"expanded_url": "https://twitter.com/AppsScriptInfo/status/1665828737768914944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 588,
+							"h": 488,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 588,
+							"h": 488,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 588,
+							"h": 488,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 322526439,
+			"id_str": "322526439",
+			"name": "AppsScriptInfo",
+			"screen_name": "AppsScriptInfo",
+			"location": "World",
+			"description": "A Google Apps Script Community account to support Google Workspace Devs. Curated by @mhawksey",
+			"url": "https://t.co/db8P5bhqPd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/db8P5bhqPd",
+							"expanded_url": "https://pulse.appsscript.info",
+							"display_url": "pulse.appsscript.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2330,
+			"friends_count": 1537,
+			"listed_count": 58,
+			"created_at": "Thu Jun 23 09:39:20 +0000 2011",
+			"favourites_count": 995,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3253,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1509531998359982080/BbebJ-ka_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1509531998359982080/BbebJ-ka_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/322526439/1559297130",
+			"profile_link_color": "4286F4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Copy macros to other Google Sheets (and how to centrally create/update Apps Script code to Google Workspace editors) (contributed by @mhawksey) #GoogleWorkspace @WorkspaceDevs #GoogleAppsScript #AppsScript https://t.co/0gGHYx8h5Q https://t.co/lEfpRDegbj"
+	},
+	{
 		"created_at": "Thu Jun 01 18:05:26 +0000 2023",
 		"id": 1664331306715455500,
 		"id_str": "1664332340775075840",
